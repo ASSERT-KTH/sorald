45aecb0daa9a596485fa3bf15d19238b83da0222
Ashutosh Kumar Verma <ashutosh1598@yahoo.com.au>
2018-06-15 21:20:48 +0200

    Made a compilation error in previous commit.

--- src/main/java/TestHelp.java
+++ src/main/java/TestHelp.java
@@ -10,29 +10,25 @@ import java.util.Set;
 import static java.lang.System.exit;
 import static java.lang.Thread.sleep;
 
-public class TestHelp
-{
+public class TestHelp {
 
     public static Map<Integer, Class<? extends Processor>> rule;
 
-    public static void initmap()
-    {
+    public static void initmap() {
         rule = new HashMap<>();
-        rule.putIfAbsent(1854,DeadStoreProcessor.class);
-        rule.putIfAbsent(1948,SerializableFieldProcessor.class);
-        rule.putIfAbsent(2055,NonSerializableSuperClassProcessor.class);
-        rule.putIfAbsent(2095,ResourceCloseProcessor.class);
-        rule.putIfAbsent(2259,NullDereferenceProcessor.class);
+        rule.putIfAbsent(1854, DeadStoreProcessor.class);
+        rule.putIfAbsent(1948, SerializableFieldProcessor.class);
+        rule.putIfAbsent(2055, NonSerializableSuperClassProcessor.class);
+        rule.putIfAbsent(2095, ResourceCloseProcessor.class);
+        rule.putIfAbsent(2259, NullDereferenceProcessor.class);
     }
-    public  static  Class<?> getProcessor(int ruleKey)
-    {
-        if(rule==null)
-        {
+
+    public static Class<?> getProcessor(int ruleKey) {
+        if (rule == null) {
             initmap();
         }
-        if(!rule.containsKey(ruleKey))
-        {
-            System.out.println("Sorry. Repair not available for rule "+ruleKey);
+        if (!rule.containsKey(ruleKey)) {
+            System.out.println("Sorry. Repair not available for rule " + ruleKey);
             exit(0);
         }
         return rule.get(ruleKey);
@@ -43,7 +39,7 @@ public class TestHelp
         //Not Sniper  Mode
         Launcher launcher = new Launcher();
 
-        launcher.addInputResource("./src/test/sonatest/"+pathToFile);
+        launcher.addInputResource("./src/test/sonatest/" + pathToFile);
 
         launcher.getEnvironment().setCommentEnabled(true);
         launcher.getEnvironment().setAutoImports(true);
@@ -54,44 +50,39 @@ public class TestHelp
         Class<?> processor = getProcessor(rulekey);
         Constructor<?> cons = processor.getConstructor(String.class);
         Object object = cons.newInstance(projectKey);
-        launcher.addProcessor( (Processor)object);
+        launcher.addProcessor((Processor) object);
         launcher.run();
 //        new SpoonModelTree(launcher.getFactory());
     }
 
-    public static boolean checkBugs(String pathToFile,int rulekey) throws Exception {
+    public static boolean checkBugs(String pathToFile, int rulekey) throws Exception {
         String cdrep = "./src/test/sonarepaired/";
         String cdtest = "./src/test/sonatest/";
 
-        String arr[]=pathToFile.split("/");
-        String fileName=arr[arr.length-1];
+        String arr[] = pathToFile.split("/");
+        String fileName = arr[arr.length - 1];
 
-        copy("./spooned/"+fileName,cdrep+pathToFile);
+        copy("./spooned/" + fileName, cdrep + pathToFile);
 
-        Execute.command("mvn clean package",cdrep,true);
+        Execute.command("mvn clean package", cdrep, true);
 
         doSonarAnalysis(cdrep);
 
-        sleep(1000*10);
+        sleep(1000 * 10);
 
-        JSONArray array = ParseAPI.parse(rulekey,"","se.kth:sonarepaired");
+        JSONArray array = ParseAPI.parse(rulekey, "", "se.kth:sonarepaired");
 
-        copy(cdtest+pathToFile,cdrep+pathToFile);//copy file back from sonatest to sonarepair
+        copy(cdtest + pathToFile, cdrep + pathToFile);//copy file back from sonatest to sonarepair
 
 //        Execute.command("rm -rf ./spooned/");
 
-        if(array.length()==0)
-        {
+        if (array.length() == 0) {
             return false;
-        }
-        else
-        {
+        } else {
             Set<Bug> setOfBugs = Bug.createSetOfBugs(array);
-            for(Bug bug : setOfBugs)
-            {
+            for (Bug bug : setOfBugs) {
                 String bugFileName = bug.getFileName();
-                if(bugFileName.equals(fileName))
-                {
+                if (bugFileName.equals(fileName)) {
                     System.out.println(bug.getJsonObject().toString());
                     return true;
                 }
@@ -101,23 +92,19 @@ public class TestHelp
 //        return (array.length()>0);
     }
 
-    public static void copy(String from,String to)
-    {
+    public static void copy(String from, String to) {
         Execute.command("cp " + from + " " + to);
     }
 
-    public static void doSonarAnalysis(String dir)
-    {
+    public static void doSonarAnalysis(String dir) {
         String sonarAnalysis = "mvn sonar:sonar   -Dsonar.organization=ashutosh1598-github   -Dsonar.host.url=https://sonarcloud.io   -Dsonar.login=2b8cc80c8434c1ea108801afa994a6a91d4facd7";
-        Execute.command(sonarAnalysis,dir,true);
+        Execute.command(sonarAnalysis, dir, true);
     }
 
-    public static boolean hasSonarBug(String pathToFile,int rulekey) throws Exception {
-        return checkBugs(pathToFile,rulekey);
+    public static boolean hasSonarBug(String pathToFile, int rulekey) throws Exception {
+        return checkBugs(pathToFile, rulekey);
     }
 }
-/*
-
         /*
         //Sniper Mode . Add pavel's refDJPP branch of spoon as library to use this.
         Launcher launcher = new Launcher();
@@ -138,4 +125,3 @@ public class TestHelp
         printer.calculate(cu, toBePrinted);
         */
 
- */
