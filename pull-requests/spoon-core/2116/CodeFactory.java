5257703f1ce22d1878c7a1f9d039de185ba87437
Haris Adzemovic <adzemovic.haris@gmail.com>
2019-10-04 16:35:29 +0200

    Actually revert CodeFactory to its unfixed stage

--- pull-requests/spoon-core/2116/CodeFactory.java
+++ pull-requests/spoon-core/2116/CodeFactory.java
@@ -199,7 +199,7 @@ public class CodeFactory extends SubFactory {
 		CtNewClass<T> ctNewClass = factory.Core().createNewClass();
 		CtConstructor<T> constructor = ((CtClass) superClass).getConstructor(Arrays.stream(parameters).map(x -> x.getType()).toArray(CtTypeReference[]::new));
 		if (constructor == null) {
-			throw new SpoonException("no appropriate constructor for these parameters " + Arrays.toString(parameters));
+			throw new SpoonException("no appropriate constructor for these parameters " + parameters.toString());
 		}
 		CtExecutableReference<T> executableReference = constructor.getReference();
 		ctNewClass.setArguments(Arrays.asList(parameters));
