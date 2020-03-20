65839dfd1cb27392ec4dfec75e85eae1ad780ba5
Fer Madeiral <fer.madeiral@gmail.com>
2020-03-20 15:30:48 +0100

    Move test from CodeFactoryTest to ArrayToStringProcessorTest (both test cases are testing the same processor)

--- src/test/java/ArrayToStringProcessorTest.java
+++ src/test/java/ArrayToStringProcessorTest.java
@@ -5,13 +5,26 @@ import org.sonar.java.checks.verifier.JavaCheckVerifier;
 public class ArrayToStringProcessorTest {
 
     @Test
-    public void test()throws Exception
+    public void arrayToStringProcessorTest()throws Exception
     {
         String fileName = "ArrayToString.java";
         String pathToRepairedFile = "./spooned/" + fileName;
 
         JavaCheckVerifier.verify(Constants.PATH_TO_FILE + fileName, new ArrayHashCodeAndToStringCheck());
         TestHelp.normalRepair(Constants.PATH_TO_FILE,Constants.PROJECT_KEY,2116);
+        TestHelp.removeComplianceComments(pathToRepairedFile);
+        JavaCheckVerifier.verifyNoIssue(pathToRepairedFile, new ArrayHashCodeAndToStringCheck());
+    }
+
+    @Test
+    public void arrayToStringProcessorTest2()throws Exception
+    {
+        String fileName = "CodeFactory.java";
+        String pathToRepairedFile = "./spooned/spoon/reflect/factory/" + fileName;
+
+        JavaCheckVerifier.verify(Constants.PATH_TO_FILE + fileName, new ArrayHashCodeAndToStringCheck());
+        TestHelp.normalRepair(Constants.PATH_TO_FILE,Constants.PROJECT_KEY,2116);
+        TestHelp.repair(Constants.PATH_TO_FILE,Constants.PROJECT_KEY,2116);
         TestHelp.removeComplianceComments(pathToRepairedFile);
         JavaCheckVerifier.verifyNoIssue(pathToRepairedFile, new ArrayHashCodeAndToStringCheck());
     }
