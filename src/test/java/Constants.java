b4031e5bd26b45db2b12080972f97bca074a4fa0
Fer Madeiral <fer.madeiral@gmail.com>
2020-03-20 15:36:43 +0100

    Simplify test resources' structure

--- src/test/java/Constants.java
+++ src/test/java/Constants.java
@@ -1,7 +1,6 @@
 public class Constants {
 
     public static final String PROJECT_KEY = "se.kth:sonatest";
-    public static final String CD_TEST ="./src/test/resources/sonatest/";
-    public static final String PATH_TO_FILE = CD_TEST + "src/main/java/";
+    public static final String PATH_TO_FILE ="./src/test/resources/";
 
 }
