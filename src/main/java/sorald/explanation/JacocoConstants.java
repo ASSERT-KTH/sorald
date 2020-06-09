d65319f40091f191b6992e9e3315c19956021227
khashayar etemadi <khaes@kth.se>
2020-06-09 13:04:36 +0200

    textual comparison added

---
+++ src/main/java/sorald/explanation/JacocoConstants.java
@@ -0,0 +1,11 @@
+package sorald.explanation;
+
+public class JacocoConstants {
+    public static final String FULL_COVERED_CLASS = "<span class=\"fc\" id=\"L";
+    public static final String PARTIAL_COVERED_CLASS = "<span class=\"nc\" id=\"L";
+    public static final String UNCOVERED_CLASS = "<span class=\"pc\" id=\"L";
+    public static final String COVERED_TO_UNCOVERED_CLASS = "<span class=\"cn\" id=\"L";
+    public static final String UNCOVERED_TO_COVERED_CLASS = "<span class=\"nc\" id=\"L";
+//    public static final String COVERED_TO_COVERED_CLASS = "<span class=\"cc\" id=\"L";
+//    public static final String UNCOVERED_TO_UNCOVERED_CLASS = "<span class=\"nn\" id=\"L";
+}
