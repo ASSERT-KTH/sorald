1e1e301b0f4b47a77333e317c4f85eb3c7cf12ef
henry-lp <33935517+henry-lp@users.noreply.github.com>
2020-07-27 13:05:08 +0200

    Feat: add segmentation algorithm (#133)

---
+++ src/main/java/sorald/segment/Pair.java
@@ -0,0 +1,19 @@
+package sorald.segment;
+
+public class Pair<T, U> {
+    public T first;
+    public U second;
+
+    public Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+}
