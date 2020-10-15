550a8ff60fc811a260fe4eaddb957fd29948ee68
Simon Larsén <slarse@slar.se>
2020-10-15 15:17:11 +0200

    Extract sonar checks list to Constants class

--- src/main/java/sorald/miner/MineSonarWarnings.java
+++ src/main/java/sorald/miner/MineSonarWarnings.java
@@ -20,161 +20,23 @@ import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.sonar.java.AnalyzerMessage;
-import org.sonar.java.checks.*;
-import org.sonar.java.checks.naming.MethodNamedEqualsCheck;
-import org.sonar.java.checks.naming.MethodNamedHashcodeOrEqualCheck;
-import org.sonar.java.checks.serialization.CustomSerializationMethodCheck;
-import org.sonar.java.checks.serialization.ExternalizableClassConstructorCheck;
-import org.sonar.java.checks.serialization.SerializableObjectInSessionCheck;
-import org.sonar.java.checks.serialization.SerializableSuperConstructorCheck;
-import org.sonar.java.checks.spring.ControllerWithSessionAttributesCheck;
-import org.sonar.java.checks.spring.SpringComponentWithWrongScopeCheck;
-import org.sonar.java.checks.spring.SpringIncompatibleTransactionalCheck;
-import org.sonar.java.checks.spring.SpringScanDefaultPackageCheck;
-import org.sonar.java.checks.synchronization.DoubleCheckedLockingCheck;
-import org.sonar.java.checks.synchronization.SynchronizationOnGetClassCheck;
-import org.sonar.java.checks.synchronization.TwoLocksWaitCheck;
-import org.sonar.java.checks.synchronization.ValueBasedObjectUsedForLockCheck;
-import org.sonar.java.checks.unused.UnusedReturnedDataCheck;
-import org.sonar.java.checks.unused.UnusedThrowableCheck;
 import org.sonar.java.checks.verifier.MultipleFilesJavaCheckVerifier;
-import org.sonar.java.se.checks.*;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import sorald.Constants;
 
 public class MineSonarWarnings {
 
-    private static final List<JavaFileScanner> SONAR_CHECKS = init();
+    private static final List<JavaFileScanner> SONAR_CHECK_INSTANCES = init();
+
 
     private static List init() {
-        List<JavaFileScanner> TEMP_SONAR_CHECKS = new ArrayList<>();
-        TEMP_SONAR_CHECKS.add(new ControllerWithSessionAttributesCheck());
-        TEMP_SONAR_CHECKS.add(new SpringScanDefaultPackageCheck());
-        TEMP_SONAR_CHECKS.add(new TwoLocksWaitCheck());
-        TEMP_SONAR_CHECKS.add(new PreparedStatementAndResultSetCheck());
-        TEMP_SONAR_CHECKS.add(new ThreadSleepCheck());
-        TEMP_SONAR_CHECKS.add(new PrintfFailCheck());
-        TEMP_SONAR_CHECKS.add(new ThreadWaitCallCheck());
-        TEMP_SONAR_CHECKS.add(new SpringIncompatibleTransactionalCheck());
-        TEMP_SONAR_CHECKS.add(new DoubleCheckedLockingCheck());
-        TEMP_SONAR_CHECKS.add(new GettersSettersOnRightFieldCheck());
-        TEMP_SONAR_CHECKS.add(new RunFinalizersCheck());
-        TEMP_SONAR_CHECKS.add(new ScheduledThreadPoolExecutorZeroCheck());
-        TEMP_SONAR_CHECKS.add(new ReuseRandomCheck());
-        TEMP_SONAR_CHECKS.add(new ObjectFinalizeOverloadedCheck());
-        TEMP_SONAR_CHECKS.add(new ReturnInFinallyCheck());
-        TEMP_SONAR_CHECKS.add(new ThreadLocalCleanupCheck());
-        TEMP_SONAR_CHECKS.add(new CompareStringsBoxedTypesWithEqualsCheck());
-        TEMP_SONAR_CHECKS.add(new InputStreamReadCheck());
-        TEMP_SONAR_CHECKS.add(new CompareToNotOverloadedCheck());
-        TEMP_SONAR_CHECKS.add(new IterableIteratorCheck());
-        TEMP_SONAR_CHECKS.add(new OverwrittenKeyCheck());
-        TEMP_SONAR_CHECKS.add(new DateFormatWeekYearCheck());
-        TEMP_SONAR_CHECKS.add(new UnusedThrowableCheck());
-        TEMP_SONAR_CHECKS.add(new CollectionSizeAndArrayLengthCheck());
-        TEMP_SONAR_CHECKS.add(new AllBranchesAreIdenticalCheck());
-        TEMP_SONAR_CHECKS.add(new SynchronizedOverrideCheck());
-        TEMP_SONAR_CHECKS.add(new ValueBasedObjectUsedForLockCheck());
-        TEMP_SONAR_CHECKS.add(new AssertOnBooleanVariableCheck());
-        TEMP_SONAR_CHECKS.add(new VolatileVariablesOperationsCheck());
-        TEMP_SONAR_CHECKS.add(new SynchronizationOnGetClassCheck());
-        TEMP_SONAR_CHECKS.add(new DoubleCheckedLockingAssignmentCheck());
-        TEMP_SONAR_CHECKS.add(new StringCallsBeyondBoundsCheck());
-        TEMP_SONAR_CHECKS.add(new RawByteBitwiseOperationsCheck());
-        TEMP_SONAR_CHECKS.add(new SyncGetterAndSetterCheck());
-        TEMP_SONAR_CHECKS.add(new StaticMultithreadedUnsafeFieldsCheck());
-        TEMP_SONAR_CHECKS.add(new NullShouldNotBeUsedWithOptionalCheck());
-        TEMP_SONAR_CHECKS.add(new DoublePrefixOperatorCheck());
-        TEMP_SONAR_CHECKS.add(new WrongAssignmentOperatorCheck());
-        TEMP_SONAR_CHECKS.add(new UnusedReturnedDataCheck());
-        TEMP_SONAR_CHECKS.add(new InappropriateRegexpCheck());
-        TEMP_SONAR_CHECKS.add(new NotifyCheck());
-        TEMP_SONAR_CHECKS.add(new SynchronizedFieldAssignmentCheck());
-        TEMP_SONAR_CHECKS.add(new SerializableObjectInSessionCheck());
-        TEMP_SONAR_CHECKS.add(new WaitInSynchronizeCheck());
-        TEMP_SONAR_CHECKS.add(new ForLoopFalseConditionCheck());
-        TEMP_SONAR_CHECKS.add(new ForLoopIncrementSignCheck());
-        TEMP_SONAR_CHECKS.add(new TransactionalMethodVisibilityCheck());
-        TEMP_SONAR_CHECKS.add(new ServletInstanceFieldCheck());
-        TEMP_SONAR_CHECKS.add(new ToStringReturningNullCheck());
-        TEMP_SONAR_CHECKS.add(new EqualsOnAtomicClassCheck());
-        TEMP_SONAR_CHECKS.add(new IgnoredReturnValueCheck());
-        TEMP_SONAR_CHECKS.add(new ConfusingOverloadCheck());
-        TEMP_SONAR_CHECKS.add(new CollectionInappropriateCallsCheck());
-        TEMP_SONAR_CHECKS.add(new SillyEqualsCheck());
-        TEMP_SONAR_CHECKS.add(new PrimitiveWrappersInTernaryOperatorCheck());
-        TEMP_SONAR_CHECKS.add(new InterruptedExceptionCheck());
-        TEMP_SONAR_CHECKS.add(new ThreadOverridesRunCheck());
-        TEMP_SONAR_CHECKS.add(new LongBitsToDoubleOnIntCheck());
-        TEMP_SONAR_CHECKS.add(new UselessIncrementCheck());
-        TEMP_SONAR_CHECKS.add(new SillyStringOperationsCheck());
-        TEMP_SONAR_CHECKS.add(new NonSerializableWriteCheck());
-        TEMP_SONAR_CHECKS.add(new ArrayHashCodeAndToStringCheck());
-        TEMP_SONAR_CHECKS.add(new CollectionCallingItselfCheck());
-        TEMP_SONAR_CHECKS.add(new BigDecimalDoubleConstructorCheck());
-        TEMP_SONAR_CHECKS.add(new InvalidDateValuesCheck());
-        TEMP_SONAR_CHECKS.add(new ReflectionOnNonRuntimeAnnotationCheck());
-        TEMP_SONAR_CHECKS.add(new CustomSerializationMethodCheck());
-        TEMP_SONAR_CHECKS.add(new ExternalizableClassConstructorCheck());
-        TEMP_SONAR_CHECKS.add(new ClassComparedByNameCheck());
-        TEMP_SONAR_CHECKS.add(new DuplicateConditionIfElseIfCheck());
-        TEMP_SONAR_CHECKS.add(new SynchronizationOnStringOrBoxedCheck());
-        TEMP_SONAR_CHECKS.add(new HasNextCallingNextCheck());
-        TEMP_SONAR_CHECKS.add(new IdenticalOperandOnBinaryExpressionCheck());
-        TEMP_SONAR_CHECKS.add(new LoopExecutingAtMostOnceCheck());
-        TEMP_SONAR_CHECKS.add(new SelfAssignementCheck());
-        TEMP_SONAR_CHECKS.add(new StringBufferAndBuilderWithCharCheck());
-        TEMP_SONAR_CHECKS.add(new MethodNamedHashcodeOrEqualCheck());
-        TEMP_SONAR_CHECKS.add(new ThreadRunCheck());
-        TEMP_SONAR_CHECKS.add(new MethodNamedEqualsCheck());
-        TEMP_SONAR_CHECKS.add(new DoubleBraceInitializationCheck());
-        TEMP_SONAR_CHECKS.add(new VolatileNonPrimitiveFieldCheck());
-        TEMP_SONAR_CHECKS.add(new ToArrayCheck());
-        TEMP_SONAR_CHECKS.add(new AbsOnNegativeCheck());
-        TEMP_SONAR_CHECKS.add(new IgnoredStreamReturnValueCheck());
-        TEMP_SONAR_CHECKS.add(new IteratorNextExceptionCheck());
-        TEMP_SONAR_CHECKS.add(new CompareToResultTestCheck());
-        TEMP_SONAR_CHECKS.add(new CastArithmeticOperandCheck());
-        TEMP_SONAR_CHECKS.add(new ShiftOnIntOrLongCheck());
-        TEMP_SONAR_CHECKS.add(new CompareToReturnValueCheck());
-        TEMP_SONAR_CHECKS.add(new ImmediateReverseBoxingCheck());
-        TEMP_SONAR_CHECKS.add(new EqualsArgumentTypeCheck());
-        TEMP_SONAR_CHECKS.add(new InnerClassOfNonSerializableCheck());
-        TEMP_SONAR_CHECKS.add(new SerializableSuperConstructorCheck());
-        TEMP_SONAR_CHECKS.add(new ParameterReassignedToCheck());
-        TEMP_SONAR_CHECKS.add(new EqualsOverridenWithHashCodeCheck());
-        TEMP_SONAR_CHECKS.add(new ObjectFinalizeOverridenCallsSuperFinalizeCheck());
-        TEMP_SONAR_CHECKS.add(new SpringComponentWithWrongScopeCheck());
-        TEMP_SONAR_CHECKS.add(new ConstructorInjectionCheck());
-        TEMP_SONAR_CHECKS.add(new ClassWithoutHashCodeInHashStructureCheck());
-        TEMP_SONAR_CHECKS.add(new InstanceOfAlwaysTrueCheck());
-        TEMP_SONAR_CHECKS.add(new NullDereferenceInConditionalCheck());
-        TEMP_SONAR_CHECKS.add(new FloatEqualityCheck());
-        TEMP_SONAR_CHECKS.add(new IfConditionAlwaysTrueOrFalseCheck());
-        TEMP_SONAR_CHECKS.add(new ObjectFinalizeCheck());
-        TEMP_SONAR_CHECKS.add(new GetClassLoaderCheck());
-        TEMP_SONAR_CHECKS.add(new MathOnFloatCheck());
-        TEMP_SONAR_CHECKS.add(new SymmetricEqualsCheck());
-
-        TEMP_SONAR_CHECKS.add(new ObjectOutputStreamCheck());
-        TEMP_SONAR_CHECKS.add(new NoWayOutLoopCheck());
-        TEMP_SONAR_CHECKS.add(new UnclosedResourcesCheck());
-        TEMP_SONAR_CHECKS.add(new DivisionByZeroCheck());
-        TEMP_SONAR_CHECKS.add(new LocksNotUnlockedCheck());
-        TEMP_SONAR_CHECKS.add(new StreamConsumedCheck());
-        TEMP_SONAR_CHECKS.add(new StreamNotConsumedCheck());
-        TEMP_SONAR_CHECKS.add(new OptionalGetBeforeIsPresentCheck());
-        TEMP_SONAR_CHECKS.add(new MinMaxRangeCheck());
-        TEMP_SONAR_CHECKS.add(new ConditionalUnreachableCodeCheck());
-        TEMP_SONAR_CHECKS.add(new NullDereferenceCheck());
-        TEMP_SONAR_CHECKS.add(new NonNullSetToNullCheck());
-        TEMP_SONAR_CHECKS.add(new CustomUnclosedResourcesCheck());
-
-//        TEMP_SONAR_CHECKS.add(new DefaultMessageListenerContainerCheck());
-//        TEMP_SONAR_CHECKS.add(new SingleConnectionFactoryCheck());
-//        TEMP_SONAR_CHECKS.add(new DependencyWithSystemScopeCheck());
-
-        return TEMP_SONAR_CHECKS;
+        return Constants.SONAR_CHECKS.stream().map(cls -> {
+            try {
+                return cls.getConstructor().newInstance();
+            } catch (Exception e) {
+                throw new RuntimeException("could not instantiate class " + cls.getName());
+            }
+        }).collect(Collectors.toList());
     }
 
     public static JSAP defineArgs() throws JSAPException {
@@ -337,7 +199,7 @@ public class MineSonarWarnings {
                     e.printStackTrace();
                 }
             }
-            for (JavaFileScanner javaFileScanner : SONAR_CHECKS) {
+            for (JavaFileScanner javaFileScanner : SONAR_CHECK_INSTANCES) {
                 Set<AnalyzerMessage> issues = MultipleFilesJavaCheckVerifier.verify(filesToScan, javaFileScanner, false);
                 warnings.putIfAbsent(javaFileScanner.getClass().getSimpleName(), issues.size());
             }
