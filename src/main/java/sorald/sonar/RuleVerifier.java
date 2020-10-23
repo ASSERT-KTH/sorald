7cc7297d571939a2ef322d76122e46cf584d854c
Simon Larsén <slarse@slar.se>
2020-10-23 15:03:41 +0200

    Add comment that we may want to set the Java version

--- src/main/java/sorald/sonar/RuleVerifier.java
+++ src/main/java/sorald/sonar/RuleVerifier.java
@@ -109,6 +109,9 @@ public class RuleVerifier {
                         Collections.emptyList(),
                         sonarComponents,
                         SymbolicExecutionMode.ENABLED);
+        // TODO we may want to set the version of the visitors bridge, as without setting it the implementation
+        //      defaults to the highest version supported by the parser (currently Java 14)
+        //      visitorsBridge.setJavaVersion(new JavaVersionImpl(8));
         scanner.setVisitorBridge(visitorsBridge);
         return scanner;
     }
