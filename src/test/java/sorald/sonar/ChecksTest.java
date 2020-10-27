950523a02524feeb34beb945ac869b050619afe8
Simon Larsén <slarse@slar.se>
2020-10-27 16:43:28 +0100

    Add rudimentary tests for Checks class

---
+++ src/test/java/sorald/sonar/ChecksTest.java
@@ -0,0 +1,43 @@
+package sorald.sonar;
+
+import org.junit.jupiter.api.Test;
+import org.sonar.java.checks.CompareStringsBoxedTypesWithEqualsCheck;
+import org.sonar.java.checks.DeadStoreCheck;
+import org.sonar.java.checks.InputStreamReadCheck;
+import org.sonar.java.checks.NullShouldNotBeUsedWithOptionalCheck;
+import org.sonar.java.checks.ObjectFinalizeCheck;
+import org.sonar.java.checks.serialization.SerializableFieldInSerializableClassCheck;
+import org.sonar.plugins.java.api.JavaFileScanner;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChecksTest {
+
+    @Test
+    void test_getChecksByType_whenTypeIsBug_containsBugChecks() {
+        final List<Class<? extends JavaFileScanner>> bugChecks =
+                Checks.getChecksByType(Checks.CheckType.BUG);
+        final List<Class<? extends JavaFileScanner>> expectedBugChecksSubset =
+                Arrays.asList(
+                        CompareStringsBoxedTypesWithEqualsCheck.class,
+                        InputStreamReadCheck.class,
+                        NullShouldNotBeUsedWithOptionalCheck.class,
+                        ObjectFinalizeCheck.class);
+
+        assertTrue(bugChecks.containsAll(expectedBugChecksSubset));
+    }
+
+    @Test
+    void test_getChecksByType_whenTypeIsCodeSmell_containsCodeSmellChecks() {
+        final List<Class<? extends JavaFileScanner>> codeSmellChecks =
+                Checks.getChecksByType(Checks.CheckType.CODE_SMELL);
+        final List<Class<? extends JavaFileScanner>> expectedCodeSmellChecksSubset =
+                Arrays.asList(
+                        DeadStoreCheck.class, SerializableFieldInSerializableClassCheck.class);
+
+        assertTrue(codeSmellChecks.containsAll(expectedCodeSmellChecksSubset));
+    }
+}
