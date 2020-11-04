e4bd8675dfe12baa2867f22f7013dd484f2f0edd
Simon Larsén <slarse@slar.se>
2020-11-04 15:34:08 +0100

    feat: Add --ruleTypes option to warnings miner (#188)
    * Add method CheckType.getLabel()
    
    * Specify behavior of --ruleTypes argument with test
    
    * Implement tentative solution for mining warnings from a specific rule type
    
    * Refactor and simplify
    
    * Change ruleTypes separator from : to ,
    
    * Reduce code duplication
    
    * Add docstring to instantiateCheck

--- src/main/java/sorald/miner/MineSonarWarnings.java
+++ src/main/java/sorald/miner/MineSonarWarnings.java
@@ -20,26 +20,11 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import sorald.Constants;
+import sorald.sonar.Checks;
 import sorald.sonar.RuleVerifier;
 import sorald.sonar.RuleViolation;
 
 public class MineSonarWarnings {
-
-    private static final List<JavaFileScanner> SONAR_CHECK_INSTANCES = init();
-
-    private static List init() {
-        return Constants.SONAR_CHECK_CLASSES.stream()
-                .map(
-                        cls -> {
-                            try {
-                                return cls.getConstructor().newInstance();
-                            } catch (Exception e) {
-                                throw new RuntimeException(
-                                        "could not instantiate class " + cls.getName());
-                            }
-                        })
-                .collect(Collectors.toList());
-    }
 
     public static JSAP defineArgs() throws JSAPException {
         JSAP jsap = new JSAP();
@@ -79,6 +64,16 @@ public class MineSonarWarnings {
         opt.setHelp("The path to the temp directory.");
         jsap.registerParameter(opt);
 
+        // TODO don't reuse the opt variable, declare one variable per option
+        opt = new FlaggedOption(Constants.ARG_RULE_TYPES);
+        opt.setLongFlag(Constants.ARG_RULE_TYPES);
+        opt.setList(true);
+        opt.setListSeparator(',');
+        opt.setRequired(false);
+        String ruleTypes = String.join(", ", Constants.SONAR_RULE_TYPES);
+        opt.setHelp("One or more types of rules to check for. Choices: " + ruleTypes);
+        jsap.registerParameter(opt);
+
         Switch sw = new Switch("help");
         sw.setShortFlag('h');
         sw.setLongFlag("help");
@@ -113,6 +108,8 @@ public class MineSonarWarnings {
         JSAP jsap = defineArgs();
         JSAPResult arguments = jsap.parse(args);
         checkArguments(jsap, arguments);
+        List<? extends JavaFileScanner> checks =
+                inferCheckInstances(arguments.getStringArray(Constants.ARG_RULE_TYPES));
 
         if (arguments.contains(Constants.ARG_STATS_ON_GIT_REPOS)) {
             // stats on a list of git repos
@@ -139,7 +136,7 @@ public class MineSonarWarnings {
                     e.printStackTrace();
                 }
 
-                Map<String, Integer> warnings = extractWarnings(repoDir.getAbsolutePath());
+                Map<String, Integer> warnings = extractWarnings(repoDir.getAbsolutePath(), checks);
 
                 PrintWriter pw = new PrintWriter(new FileWriter(outputPath, true));
 
@@ -162,7 +159,7 @@ public class MineSonarWarnings {
             String projectPath =
                     arguments.getFile(Constants.ARG_ORIGINAL_FILES_PATH).getAbsolutePath();
 
-            Map<String, Integer> warnings = extractWarnings(projectPath);
+            Map<String, Integer> warnings = extractWarnings(projectPath, checks);
 
             warnings.entrySet().stream()
                     .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
@@ -190,7 +187,8 @@ public class MineSonarWarnings {
      * @param checks Checks to run on the Java files in the project
      * @return A mapping (checkClassName -> numViolations)
      */
-    static Map<String, Integer> extractWarnings(String projectPath, List<JavaFileScanner> checks) {
+    static Map<String, Integer> extractWarnings(
+            String projectPath, List<? extends JavaFileScanner> checks) {
         List<String> filesToScan = new ArrayList<>();
         File file = new File(projectPath);
         if (file.isFile()) {
@@ -220,7 +218,30 @@ public class MineSonarWarnings {
         return warnings;
     }
 
-    private static Map<String, Integer> extractWarnings(String projectPath) {
-        return extractWarnings(projectPath, SONAR_CHECK_INSTANCES);
+    /**
+     * Infer which check instances to use based on rule types specified (or left unspecified) on the
+     * command line.
+     */
+    private static List<? extends JavaFileScanner> inferCheckInstances(String[] ruleTypes) {
+        List<Checks.CheckType> checkTypes =
+                Arrays.stream(ruleTypes)
+                        .map(Checks.CheckType::fromLabel)
+                        .collect(Collectors.toList());
+        return checkTypes.isEmpty() ? getAllCheckInstances() : getCheckInstancesByTypes(checkTypes);
+    }
+
+    private static List<? extends JavaFileScanner> getCheckInstancesByTypes(
+            List<Checks.CheckType> checkTypes) {
+        return checkTypes.stream()
+                .map(Checks::getChecksByType)
+                .flatMap(Collection::stream)
+                .map(Checks::instantiateCheck)
+                .collect(Collectors.toList());
+    }
+
+    private static List<? extends JavaFileScanner> getAllCheckInstances() {
+        return Checks.getAllChecks().stream()
+                .map(Checks::instantiateCheck)
+                .collect(Collectors.toList());
     }
 }
