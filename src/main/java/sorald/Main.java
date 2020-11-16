ede8271986cc420ff558e6548e551e6a2ef7442e
Simon Larsén <slarse@slar.se>
2020-11-16 08:29:24 +0100

    Add message to CLI exception

--- src/main/java/sorald/Main.java
+++ src/main/java/sorald/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) throws Exception {
         int exitStatus = Cli.createCli().execute(args);
 
-        // TODO change this behavior, we don't actually want to throw an exception in the CLI
+        // TODO change this behavior to just exit with the status, we don't actually want to throw
+        //  an exception in the CLI
         if (exitStatus != 0) {
-            throw new IllegalStateException();
+            throw new IllegalStateException("CLI exited non-zero");
         }
     }
 }
