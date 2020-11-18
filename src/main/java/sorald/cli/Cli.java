d6131f986b274a55be72577d4e023dbbd8057e74
Simon Larsén <slarse@slar.se>
2020-11-18 11:55:30 +0100

    Incorporate both miner and repair as subcommands of the CLI

--- src/main/java/sorald/cli/Cli.java
+++ src/main/java/sorald/cli/Cli.java
@@ -1,9 +1,15 @@
 package sorald.cli;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
+
+import org.sonar.plugins.java.api.JavaFileScanner;
 import picocli.CommandLine;
 import sorald.Constants;
 import sorald.DefaultRepair;
@@ -14,26 +20,57 @@ import sorald.RepairStrategy;
 import sorald.SegmentRepair;
 import sorald.SoraldAbstractRepair;
 import sorald.SoraldConfig;
+import sorald.miner.MineSonarWarnings;
 import sorald.segment.FirstFitSegmentationAlgorithm;
 import sorald.segment.Node;
 import sorald.segment.SoraldTreeBuilderAlgorithm;
+import sorald.sonar.Checks;
 
 /** Class containing the CLI for Sorald. */
 public class Cli {
 
     /** @return Sorald's command line interface. */
     public static CommandLine createCli() {
-        return new CommandLine(new RepairCommand());
+        return new CommandLine(new SoraldCLI());
+    }
+
+    @CommandLine.Command(
+            mixinStandardHelpOptions = true,
+            subcommands = {RepairCommand.class, MineCommand.class},
+            description =
+                    "The Sorald command line application for automatic repair of Sonar rule violations.",
+            synopsisSubcommandLabel = "<COMMAND>")
+    static class SoraldCLI implements Callable<Integer> {
+
+        @Override
+        public Integer call() {
+            new CommandLine(this).usage(System.out);
+            return 1;
+        }
+    }
+
+    /**
+     * Abstract command base class with the options that are in common for both the miner and repair
+     * tool
+     */
+    private abstract static class CommandBase {
+        @CommandLine.Spec CommandLine.Model.CommandSpec spec;
+
+        @CommandLine.Option(
+                names = {Constants.ARG_SYMBOL + Constants.ARG_ORIGINAL_FILES_PATH},
+                description =
+                        "The path to the file or folder to be analyzed and possibly repaired.",
+                required = true)
+        File originalFilesPath;
     }
 
     /** The CLI command for the primary repair application. */
     @CommandLine.Command(
+            name = Constants.REPAIR_COMMAND_NAME,
             mixinStandardHelpOptions = true,
-            description = "Sorald automatic repair tool.")
-    private static class RepairCommand implements Callable<Integer> {
+            description = "Repair Sonar rule violations in a targeted project.")
+    private static class RepairCommand extends CommandBase implements Callable<Integer> {
         private List<Integer> ruleKeys;
-
-        @CommandLine.Spec CommandLine.Model.CommandSpec spec;
 
         @CommandLine.Option(
                 names = {Constants.ARG_SYMBOL + Constants.ARG_RULE_KEYS},
@@ -55,13 +92,6 @@ public class Cli {
             }
             ruleKeys = value;
         }
-
-        @CommandLine.Option(
-                names = {Constants.ARG_SYMBOL + Constants.ARG_ORIGINAL_FILES_PATH},
-                description =
-                        "The path to the file or folder to be analyzed and possibly repaired.",
-                required = true)
-        File originalFilesPath;
 
         @CommandLine.Option(
                 names = {Constants.ARG_SYMBOL + Constants.ARG_WORKSPACE},
@@ -154,6 +184,79 @@ public class Cli {
                 repair = new DefaultRepair(config);
             }
             return repair;
+        }
+    }
+
+    @CommandLine.Command(
+            name = Constants.MINE_COMMAND_NAME,
+            mixinStandardHelpOptions = true,
+            description = "Mine a project for Sonar warnings.")
+    private static class MineCommand extends CommandBase implements Callable<Integer> {
+
+        @CommandLine.Option(
+                names = Constants.ARG_SYMBOL + Constants.ARG_STATS_ON_GIT_REPOS,
+                description = "If the stats should be computed on git repos.")
+        boolean statsOnGitRepos;
+
+        @CommandLine.Option(
+                names = Constants.ARG_SYMBOL + Constants.ARG_STATS_OUTPUT_FILE,
+                description = "The path to the output file.")
+        File statsOutputFile;
+
+        @CommandLine.Option(
+                names = Constants.ARG_SYMBOL + Constants.ARG_GIT_REPOS_LIST,
+                description = "The path to the repos list.")
+        File reposList;
+
+        @CommandLine.Option(
+                names = Constants.ARG_SYMBOL + Constants.ARG_TEMP_DIR,
+                description = "The path to the temp directory.")
+        File tempDir;
+
+        @CommandLine.Option(
+                names = {Constants.ARG_SYMBOL + Constants.ARG_RULE_TYPES},
+                description =
+                        "One or more types of rules to check for (use ',' to separate multiple types). Choices: ${COMPLETION-CANDIDATES}",
+                split = ",")
+        private List<Checks.CheckType> ruleTypes = new ArrayList<>();
+
+        @Override
+        public Integer call() throws Exception {
+            List<? extends JavaFileScanner> checks = inferCheckInstances(ruleTypes);
+            if (statsOnGitRepos) {
+                List<String> reposList = Files.readAllLines(this.reposList.toPath());
+                MineSonarWarnings.mineGitRepos(
+                        checks, statsOutputFile.getAbsolutePath(), reposList, tempDir);
+            } else {
+                MineSonarWarnings.mineLocalProject(checks, originalFilesPath.getAbsolutePath());
+            }
+            return 0;
+        }
+
+        /**
+         * Infer which check instances to use based on rule types specified (or left unspecified) on
+         * the command line.
+         */
+        private static List<? extends JavaFileScanner> inferCheckInstances(
+                List<Checks.CheckType> ruleTypes) {
+            return ruleTypes.isEmpty()
+                    ? getAllCheckInstances()
+                    : getCheckInstancesByTypes(ruleTypes);
+        }
+
+        private static List<? extends JavaFileScanner> getCheckInstancesByTypes(
+                List<Checks.CheckType> checkTypes) {
+            return checkTypes.stream()
+                    .map(Checks::getChecksByType)
+                    .flatMap(Collection::stream)
+                    .map(Checks::instantiateCheck)
+                    .collect(Collectors.toList());
+        }
+
+        private static List<? extends JavaFileScanner> getAllCheckInstances() {
+            return Checks.getAllChecks().stream()
+                    .map(Checks::instantiateCheck)
+                    .collect(Collectors.toList());
         }
     }
 }
--- src/main/java/sorald/miner/MineSonarWarnings.java
+++ src/main/java/sorald/miner/MineSonarWarnings.java
@@ -1,13 +1,11 @@
 package sorald.miner;
 
-import com.martiansoftware.jsap.FlaggedOption;
 import com.martiansoftware.jsap.JSAP;
 import com.martiansoftware.jsap.JSAPException;
-import com.martiansoftware.jsap.JSAPResult;
-import com.martiansoftware.jsap.Switch;
-import com.martiansoftware.jsap.stringparsers.BooleanStringParser;
-import com.martiansoftware.jsap.stringparsers.FileStringParser;
+
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
@@ -15,82 +13,10 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.sonar.plugins.java.api.JavaFileScanner;
 import sorald.Constants;
-import sorald.sonar.Checks;
 import sorald.sonar.RuleVerifier;
 import sorald.sonar.RuleViolation;
 
 public class MineSonarWarnings {
-
-    public static JSAP defineArgs() throws JSAPException {
-        JSAP jsap = new JSAP();
-
-        FlaggedOption opt = new FlaggedOption(Constants.ARG_ORIGINAL_FILES_PATH);
-        opt.setLongFlag(Constants.ARG_ORIGINAL_FILES_PATH);
-        opt.setStringParser(FileStringParser.getParser().setMustExist(true));
-        opt.setRequired(false);
-        opt.setHelp("The path to the file or folder to be analyzed.");
-        jsap.registerParameter(opt);
-
-        opt = new FlaggedOption(Constants.ARG_STATS_ON_GIT_REPOS);
-        opt.setLongFlag(Constants.ARG_STATS_ON_GIT_REPOS);
-        opt.setStringParser(BooleanStringParser.getParser());
-        opt.setRequired(false);
-        opt.setHelp("If the stats should be computed on git repos.");
-        jsap.registerParameter(opt);
-
-        opt = new FlaggedOption(Constants.ARG_STATS_OUTPUT_FILE);
-        opt.setLongFlag(Constants.ARG_STATS_OUTPUT_FILE);
-        opt.setStringParser(FileStringParser.getParser().setMustExist(true));
-        opt.setRequired(false);
-        opt.setHelp("The path to the output file.");
-        jsap.registerParameter(opt);
-
-        opt = new FlaggedOption(Constants.ARG_GIT_REPOS_LIST);
-        opt.setLongFlag(Constants.ARG_GIT_REPOS_LIST);
-        opt.setStringParser(FileStringParser.getParser().setMustExist(true));
-        opt.setRequired(false);
-        opt.setHelp("The path to the repos list.");
-        jsap.registerParameter(opt);
-
-        opt = new FlaggedOption(Constants.ARG_TEMP_DIR);
-        opt.setLongFlag(Constants.ARG_TEMP_DIR);
-        opt.setStringParser(FileStringParser.getParser().setMustExist(true));
-        opt.setRequired(false);
-        opt.setHelp("The path to the temp directory.");
-        jsap.registerParameter(opt);
-
-        // TODO don't reuse the opt variable, declare one variable per option
-        opt = new FlaggedOption(Constants.ARG_RULE_TYPES);
-        opt.setLongFlag(Constants.ARG_RULE_TYPES);
-        opt.setList(true);
-        opt.setListSeparator(',');
-        opt.setRequired(false);
-        String ruleTypes = String.join(", ", Constants.SONAR_RULE_TYPES);
-        opt.setHelp("One or more types of rules to check for. Choices: " + ruleTypes);
-        jsap.registerParameter(opt);
-
-        Switch sw = new Switch("help");
-        sw.setShortFlag('h');
-        sw.setLongFlag("help");
-        sw.setDefault("false");
-        jsap.registerParameter(sw);
-
-        return jsap;
-    }
-
-    public static void checkArguments(JSAP jsap, JSAPResult arguments) {
-        if (!arguments.success()) {
-            for (java.util.Iterator<?> errors = arguments.getErrorMessageIterator();
-                    errors.hasNext(); ) {
-                System.err.println("Error: " + errors.next());
-            }
-            printUsage(jsap);
-        }
-
-        if (arguments.getBoolean("help")) {
-            printUsage(jsap);
-        }
-    }
 
     public static void printUsage(JSAP jsap) {
         System.err.println("Arguments: ");
@@ -99,82 +25,48 @@ public class MineSonarWarnings {
         System.exit(-1);
     }
 
-    public static void main(String[] args) throws JSAPException, IOException, GitAPIException {
-        JSAP jsap = defineArgs();
-        JSAPResult arguments = jsap.parse(args);
-        checkArguments(jsap, arguments);
-        List<? extends JavaFileScanner> checks =
-                inferCheckInstances(arguments.getStringArray(Constants.ARG_RULE_TYPES));
+    public static void mineGitRepos(List<? extends JavaFileScanner> checks, String outputPath, List<String> reposList, File repoDir) throws IOException {
+        // stats on a list of git repos
+        for (String repo : reposList) {
+            String repoName = repo.substring(repo.lastIndexOf('/') + 1, repo.lastIndexOf("."));
 
-        if (arguments.contains(Constants.ARG_STATS_ON_GIT_REPOS)) {
-            // stats on a list of git repos
-            String outputPath =
-                    arguments.getFile(Constants.ARG_STATS_OUTPUT_FILE).getAbsolutePath();
-            String reposListFilePath =
-                    arguments.getFile(Constants.ARG_GIT_REPOS_LIST).getAbsolutePath();
-            File repoDir = new File(arguments.getFile(Constants.ARG_TEMP_DIR).getAbsolutePath());
+            org.apache.commons.io.FileUtils.cleanDirectory(repoDir);
 
-            List<String> reposList = getReposList(reposListFilePath);
+            boolean isCloned = false;
 
-            for (String repo : reposList) {
-                String repoName = repo.substring(repo.lastIndexOf('/') + 1, repo.lastIndexOf("."));
-
-                org.apache.commons.io.FileUtils.cleanDirectory(repoDir);
-
-                boolean isCloned = false;
-
-                try {
-                    Git git = Git.cloneRepository().setURI(repo).setDirectory(repoDir).call();
-                    git.close();
-                    isCloned = true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                Map<String, Integer> warnings = extractWarnings(repoDir.getAbsolutePath(), checks);
-
-                PrintWriter pw = new PrintWriter(new FileWriter(outputPath, true));
-
-                if (isCloned) {
-                    pw.println("RepoName: " + repoName);
-
-                    warnings.entrySet().stream()
-                            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                            .forEach(pw::println);
-                } else {
-                    pw.println("RepoName: " + repoName + " not_cloned");
-                }
-
-                pw.flush();
-                pw.close();
+            try {
+                Git git = Git.cloneRepository().setURI(repo).setDirectory(repoDir).call();
+                git.close();
+                isCloned = true;
+            } catch (Exception e) {
+                e.printStackTrace();
             }
 
-        } else { // default mode
+            Map<String, Integer> warnings = extractWarnings(repoDir.getAbsolutePath(), checks);
 
-            String projectPath =
-                    arguments.getFile(Constants.ARG_ORIGINAL_FILES_PATH).getAbsolutePath();
+            PrintWriter pw = new PrintWriter(new FileWriter(outputPath, true));
 
-            Map<String, Integer> warnings = extractWarnings(projectPath, checks);
+            if (isCloned) {
+                pw.println("RepoName: " + repoName);
 
-            warnings.entrySet().stream()
-                    .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
-                    .forEach(System.out::println);
+                warnings.entrySet().stream()
+                        .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                        .forEach(pw::println);
+            } else {
+                pw.println("RepoName: " + repoName + " not_cloned");
+            }
+
+            pw.flush();
+            pw.close();
         }
     }
 
-    private static List<String> getReposList(String reposListFilePath)
-            throws FileNotFoundException {
-        List<String> res = new ArrayList<>();
+    public static void mineLocalProject(List<? extends JavaFileScanner> checks, String projectPath) {
+        Map<String, Integer> warnings = extractWarnings(projectPath, checks);
 
-        Scanner sc = new Scanner(new File(reposListFilePath));
-
-        while (sc.hasNextLine()) {
-            res.add(sc.nextLine());
-        }
-
-        sc.close();
-
-        return res;
+        warnings.entrySet().stream()
+                .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+                .forEach(System.out::println);
     }
 
     /**
@@ -211,32 +103,5 @@ public class MineSonarWarnings {
                 .map(RuleViolation::getCheckName)
                 .forEach(incrementWarningCount);
         return warnings;
-    }
-
-    /**
-     * Infer which check instances to use based on rule types specified (or left unspecified) on the
-     * command line.
-     */
-    private static List<? extends JavaFileScanner> inferCheckInstances(String[] ruleTypes) {
-        List<Checks.CheckType> checkTypes =
-                Arrays.stream(ruleTypes)
-                        .map(Checks.CheckType::fromLabel)
-                        .collect(Collectors.toList());
-        return checkTypes.isEmpty() ? getAllCheckInstances() : getCheckInstancesByTypes(checkTypes);
-    }
-
-    private static List<? extends JavaFileScanner> getCheckInstancesByTypes(
-            List<Checks.CheckType> checkTypes) {
-        return checkTypes.stream()
-                .map(Checks::getChecksByType)
-                .flatMap(Collection::stream)
-                .map(Checks::instantiateCheck)
-                .collect(Collectors.toList());
-    }
-
-    private static List<? extends JavaFileScanner> getAllCheckInstances() {
-        return Checks.getAllChecks().stream()
-                .map(Checks::instantiateCheck)
-                .collect(Collectors.toList());
     }
 }
