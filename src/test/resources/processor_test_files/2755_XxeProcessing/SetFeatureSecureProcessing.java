365f2769bd3e85ae67c7d00d362eaaabfc2bd913
Simon Larsén <slarse@slar.se>
2020-11-23 09:29:05 +0100

    Revert "Add thrown exceptions to setFeature tests"
    This reverts commit cebe6cb58217e095e970e761b4852086eb6b44aa.

--- src/test/resources/processor_test_files/2755_XxeProcessing/SetFeatureSecureProcessing.java
+++ src/test/resources/processor_test_files/2755_XxeProcessing/SetFeatureSecureProcessing.java
@@ -1,9 +1,4 @@
-import org.w3c.dom.Document;
-import org.xml.sax.InputSource;
-
 import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
@@ -20,12 +15,5 @@ public class SetFeatureSecureProcessing {
         StringWriter writer = new StringWriter();
         transformer.transform(new StreamSource(xml), new StreamResult(writer));
         return writer.toString();
-    }
-
-    public static Document parse(String xmlFile) throws Exception {
-        DocumentBuilderFactory df = DocumentBuilderFactory.newInstance();  // Noncompliant
-        df.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-        DocumentBuilder builder = df.newDocumentBuilder();
-        return builder.parse(new InputSource(xmlFile));
     }
 }
