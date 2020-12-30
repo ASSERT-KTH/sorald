f0eea76ae0831e64747601c6e454c4ded8204a0b
khashayar etemadi <khaes@kth.se>
2020-12-30 21:55:43 +0100

    processor added

---
+++ src/main/java/sorald/processor/ToStringReturningNullProcessor.java
@@ -0,0 +1,30 @@
+package sorald.processor;
+
+import sorald.Constants;
+import sorald.annotations.IncompleteProcessor;
+import sorald.annotations.ProcessorAnnotation;
+import spoon.reflect.code.CtReturn;
+import spoon.reflect.declaration.CtMethod;
+import spoon.reflect.visitor.filter.TypeFilter;
+
+@IncompleteProcessor(description = "does not fix null returning clone()")
+@ProcessorAnnotation(
+        key = 2225,
+        description = "\"toString()\" and \"clone()\" methods should not return null")
+public class ToStringReturningNullProcessor extends SoraldAbstractProcessor<CtReturn<?>>{
+
+    @Override
+    protected boolean canRepairInternal(CtReturn<?> candidate) {
+        CtMethod parentMethod = candidate.getParent(new TypeFilter<>(CtMethod.class));
+        if(parentMethod.getSignature().equals(Constants.TOSTRING_METHOD_NAME + "()")
+                && candidate.getReturnedExpression().toString().equals("null")){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void repairInternal(CtReturn<?> element) {
+        element.getReturnedExpression().replace(getFactory().Code().createLiteral(""));
+    }
+}
