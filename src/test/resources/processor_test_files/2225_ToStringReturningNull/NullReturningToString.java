868ef7f86b7acdd57f10f5940128bb2e3a5067e1
khashayar etemadi <khaes@kth.se>
2020-12-31 21:39:24 +0100

    add noncompliant comment

--- src/test/resources/processor_test_files/2225_ToStringReturningNull/NullReturningToString.java
+++ src/test/resources/processor_test_files/2225_ToStringReturningNull/NullReturningToString.java
@@ -7,7 +7,7 @@ public class NullReturningToString {
     public String toString() {
         Random r = new Random();
         if(r.nextInt(10) == r.nextInt(10)){
-            return null;
+            return null; // Noncompliant
         }
         else if(r.nextInt(10) == r.nextInt(10)){
             return "null";
