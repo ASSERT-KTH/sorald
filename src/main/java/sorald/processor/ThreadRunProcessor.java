c71807691a30ab729f9ac5a471ae5c80e8853c7a
khashayar etemadi <khaes@kth.se>
2021-01-05 21:21:23 +0100

    review suggestions applied

--- src/main/java/sorald/processor/ThreadRunProcessor.java
+++ src/main/java/sorald/processor/ThreadRunProcessor.java
@@ -7,9 +7,9 @@ import spoon.reflect.declaration.CtMethod;
 import spoon.reflect.factory.Factory;
 
 @ProcessorAnnotation(key = 1217, description = "\"Thread.run()\" should not be called directly")
-public class ThreadRunProcessor extends SoraldAbstractProcessor<CtInvocation> {
+public class ThreadRunProcessor extends SoraldAbstractProcessor<CtInvocation<?>> {
     @Override
-    protected boolean canRepairInternal(CtInvocation candidate) {
+    protected boolean canRepairInternal(CtInvocation<?> candidate) {
         return candidate.getExecutable() != null
                 && candidate.getExecutable().getDeclaringType() != null
                 && candidate.getExecutable().getSignature().equals("run()")
@@ -21,13 +21,13 @@ public class ThreadRunProcessor extends SoraldAbstractProcessor<CtInvocation> {
     }
 
     @Override
-    protected void repairInternal(CtInvocation element) {
+    protected void repairInternal(CtInvocation<?> element) {
         Factory factory = element.getFactory();
         CtClass<?> threadClass = factory.Class().get(Thread.class);
 
         CtMethod<?> method = threadClass.getMethodsByName("start").get(0);
 
-        CtInvocation threadStartInvocation =
+        CtInvocation<?> threadStartInvocation =
                 factory.createInvocation(element.getTarget(), method.getReference());
 
         element.replace(threadStartInvocation);
