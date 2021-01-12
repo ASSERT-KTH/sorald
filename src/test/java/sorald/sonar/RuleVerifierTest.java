ec0626af8e986caa00def35a4543297b70bd18fa
Simon Larsén <slarse@slar.se>
2021-01-12 10:27:18 +0100

    Add test for SuppressWarnings on method

--- src/test/java/sorald/sonar/RuleVerifierTest.java
+++ src/test/java/sorald/sonar/RuleVerifierTest.java
@@ -2,11 +2,15 @@ package sorald.sonar;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.in;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.JavaFileScanner;
@@ -14,6 +18,31 @@ import org.sonar.plugins.java.api.JavaFileScannerContext;
 import sorald.Constants;
 
 class RuleVerifierTest {
+
+    @Test
+    public void analyze_filtersOutMessages_thatCorrespondToMethodSuppressedWarning()
+            throws IllegalAccessException, InstantiationException {
+        String suppressedRuleKey = "2116";
+        String nonSuppressedRuleKey = "2164";
+        String testFile =
+                Paths.get(Constants.PATH_TO_RESOURCES_FOLDER)
+                        .resolve("WithMethodSuppressedS" + suppressedRuleKey + ".java")
+                        .toString();
+
+        var violations =
+                RuleVerifier.analyze(
+                        List.of(testFile),
+                        new File(Constants.PATH_TO_RESOURCES_FOLDER),
+                        List.of(
+                                Checks.getCheckInstance(suppressedRuleKey),
+                                Checks.getCheckInstance(nonSuppressedRuleKey)));
+
+        assertThat(violations, is(not(empty())));
+        Set<String> violatedRuleKeys =
+                violations.stream().map(RuleViolation::getRuleKey).collect(Collectors.toSet());
+        assertThat(nonSuppressedRuleKey, in(violatedRuleKeys));
+        assertThat(suppressedRuleKey, not(in(violatedRuleKeys)));
+    }
 
     @Test
     public void analyze_filtersOutMessages_thatLackPrimaryLocation() {
