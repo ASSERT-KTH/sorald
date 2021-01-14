85b3c319d79f898f065d5e8d6cd4fa7e1e3f5327
Simon Larsén <slarse@slar.se>
2021-01-14 18:30:24 +0100

    Add Spoon generation code

---
+++ src/main/java/sorald/GenerateCode.java
@@ -0,0 +1,22 @@
+package sorald;
+
+import sorald.annotations.ProcessorsClassGenerator;
+import spoon.Launcher;
+
+/** Wrapper class for all (present and future) code generation in Sorald. */
+public class GenerateCode {
+    public static void main(String[] args) {
+        final Launcher launcher = new Launcher();
+        launcher.getEnvironment().setAutoImports(true);
+        launcher.getEnvironment().setNoClasspath(true);
+        launcher.getEnvironment().setCommentEnabled(true);
+        launcher.setSourceOutputDirectory("./src/main/java");
+
+        launcher.addInputResource("src/main/java/sorald");
+
+        launcher.addProcessor(new ProcessorsClassGenerator<>());
+        launcher.setOutputFilter("sorald.Processors");
+
+        launcher.run();
+    }
+}
