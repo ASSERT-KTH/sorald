d04c9554dcced444f0ee7cba66c7d5f0d751a686
Simon Larsén <slarse@slar.se>
2021-01-14 11:55:52 +0100

    Add setup.py

---
+++ experimentation/tools/setup.py
@@ -0,0 +1,23 @@
+import re
+from setuptools import setup, find_packages
+
+test_requirements = ["pytest>=6.0.0"]
+required = [
+    "jinja2>=2.11.2",
+    "gitpython",
+    "pygithub",
+    "requests",
+    "tqdm>=4.51.0",
+    "pandas>=1.1.3",
+]
+
+setup(
+    name="soraldscripts",
+    description="Helper scripts for the Sorald automatic repair tool",
+    packages=find_packages(exclude=("tests", "docs")),
+    tests_require=test_requirements,
+    install_requires=required,
+    extras_require=dict(TEST=test_requirements),
+    include_package_data=True,
+    zip_safe=False,
+)
