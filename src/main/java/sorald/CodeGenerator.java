d66fa5632e19e5c4ea522a973057a07f03c1ade9
Simon Larsén <slarse@slar.se>
2021-01-15 08:09:56 +0100

    Refactor CodeGenerator

--- src/main/java/sorald/CodeGenerator.java
+++ src/main/java/sorald/CodeGenerator.java
@@ -5,13 +5,18 @@ import spoon.Launcher;
 
 /** Wrapper class for all (present and future) code generation in Sorald. */
 public class CodeGenerator {
+
     public static void main(String[] args) {
+        generateSources();
+    }
+
+    private static void generateSources() {
         final Launcher launcher = new Launcher();
         launcher.getEnvironment().setAutoImports(true);
         launcher.getEnvironment().setNoClasspath(true);
         launcher.getEnvironment().setCommentEnabled(true);
-        launcher.setSourceOutputDirectory("./src/main/java");
 
+        launcher.setSourceOutputDirectory("./src/main/java");
         launcher.addInputResource("src/main/java/sorald");
 
         launcher.addProcessor(new ProcessorsClassGenerator<>());
