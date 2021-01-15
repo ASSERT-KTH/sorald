f2ef000d65c427d54709e81bf5a345fe252a6db2
Simon Larsén <slarse@slar.se>
2021-01-15 08:39:59 +0100

    Add note about Processors being partially generated

--- src/main/java/sorald/Processors.java
+++ src/main/java/sorald/Processors.java
@@ -3,7 +3,12 @@ package sorald;
 import java.util.Map;
 import sorald.processor.*;
 
+/**
+ * This class is partially generated. It is fine to edit non-generated code as per usual, but don't
+ * change any of the generated fields unless you know precisely what you are doing.
+ */
 public class Processors {
+    // GENERATED FIELD
     private static final Map<Integer, Class<? extends SoraldAbstractProcessor<?>>>
             RULE_KEY_TO_PROCESSOR =
                     new java.util.HashMap<>() {
@@ -32,6 +37,7 @@ public class Processors {
                         }
                     };
 
+    // GENERATED FIELD
     public static final String RULE_DESCRIPTIONS =
             "1217: \"Thread.run()\" should not be called directly\n1444: \"public static\" fields should be constant\n\t(incomplete: does not fix variable naming)\n1656: Variables should not be self-assigned\n1854: Unused assignments should be removed\n1860: Synchronization should not be based on Strings or boxed primitives\n1948: Fields in a \"Serializable\" class should either be transient or serializable\n2095: Resources should be closed\n2111: \"BigDecimal(double)\" should not be used\n2116: \"hashCode\" and \"toString\" should not be called on array instances\n2142: \"InterruptedException\" should not be ignored\n2164: Math should not be performed on floats\n2167: \"compareTo\" should not return \"Integer.MIN_VALUE\"\n2184: Math operands should be cast before assignment\n2204: \".equals()\" should not be used to test the values of \"Atomic\" classes\n2225: \"toString()\" and \"clone()\" methods should not return null\n\t(incomplete: does not fix null returning clone())\n2272: \"Iterator.next()\" methods should throw \"NoSuchElementException\"\n2755: XML parsers should not be vulnerable to XXE attacks\n\t(incomplete: This processor is a WIP and currently supports a subset of rule 2755. See Sorald\'s documentation for details.)\n3032: JEE applications should not \"getClassLoader\"\n3067: \"getClass\" should not be used for synchronization\n3984: Exception should not be created without being thrown\n4973: Strings and Boxed types should be compared using \"equals()\"";
 
