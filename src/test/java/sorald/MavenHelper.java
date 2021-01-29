62e288423c8350e3670c1ba918c37ca9b4f816e1
Simon Larsén <slarse@slar.se>
2021-01-29 11:24:45 +0100

    Make convertToMaven more general

--- src/test/java/sorald/MavenHelper.java
+++ src/test/java/sorald/MavenHelper.java
@@ -5,56 +5,57 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import sorald.processor.ProcessorTestHelper;
 
 public class MavenHelper {
     static void convertToMavenProject(File projectDir) throws IOException {
-        List<File> packageDirs = new ArrayList<>();
+        List<File> javaFiles = FileUtils.findFilesByExtension(projectDir, Constants.JAVA_EXT);
 
-        for (File testDir : projectDir.listFiles()) {
-            if (!testDir.isDirectory()) {
+        Path productionSourceDir = projectDir.toPath().resolve("src/main/java");
+
+        for (File javaFile : javaFiles) {
+            if (ProcessorTestHelper.isIgnoredTestFile(javaFile)
+                    || !ProcessorTestHelper.isStandaloneCompilableTestFile(javaFile)) {
+                boolean deleted = javaFile.delete();
+                assertTrue(deleted);
                 continue;
             }
 
-            String dirNameWithoutPrefix = testDir.getName().substring(5);
-            File mavenTestDir = testDir.toPath().resolveSibling(dirNameWithoutPrefix).toFile();
-            boolean renamed = testDir.renameTo(mavenTestDir);
-            assertTrue(renamed);
-            packageDirs.add(mavenTestDir);
+            Path packagePath = projectDir.toPath().relativize(javaFile.getParentFile().toPath());
+            List<String> sanitizedPathComponents =
+                    StreamSupport.stream(packagePath.spliterator(), false)
+                            .map(path -> path.toString().replaceAll("[^a-zA-Z]", ""))
+                            .collect(Collectors.toList());
 
-            for (File javaFile : mavenTestDir.listFiles()) {
-                if (!javaFile.getName().endsWith(Constants.JAVA_EXT)) {
-                    continue;
-                } else if (ProcessorTestHelper.isIgnoredTestFile(javaFile)
-                        || !ProcessorTestHelper.isStandaloneCompilableTestFile(javaFile)) {
-                    boolean deleted = javaFile.delete();
-                    assertTrue(deleted);
-                    continue;
-                }
+            String packageName = String.join(".", sanitizedPathComponents);
+            String contentWithoutPackage =
+                    Files.readAllLines(javaFile.toPath()).stream()
+                            .filter(line -> !line.matches("package .*;"))
+                            .collect(Collectors.joining("\n"));
+            String packageStatement =
+                    packageName.isEmpty() ? "" : String.format("package %s;\n", packageName);
+            Files.writeString(javaFile.toPath(), packageStatement + contentWithoutPackage);
 
-                String contentWithoutPackage =
-                        Files.readAllLines(javaFile.toPath()).stream()
-                                .filter(line -> !line.matches("package .*;"))
-                                .collect(Collectors.joining("\n"));
-                String packageStatement = String.format("package %s;\n", dirNameWithoutPrefix);
-                Files.writeString(javaFile.toPath(), packageStatement + contentWithoutPackage);
+            Path packageRelPath = Paths.get(String.join(File.separator, sanitizedPathComponents));
+            Path packageAbsPath = productionSourceDir.resolve(packageRelPath);
+
+            if (!packageAbsPath.toFile().isDirectory()) {
+                boolean created = packageAbsPath.toFile().mkdirs();
+                assertTrue(created);
             }
+
+            boolean moved = javaFile.renameTo(packageAbsPath.resolve(javaFile.getName()).toFile());
+            assertTrue(moved);
         }
-
-        File srcDir = projectDir.toPath().resolve("src/main/java").toFile();
-        boolean dirCreated = srcDir.mkdirs();
-        assertTrue(dirCreated);
-
-        packageDirs.forEach(
-                pkgDir -> pkgDir.renameTo(srcDir.toPath().resolve(pkgDir.getName()).toFile()));
 
         File pomFile =
                 Paths.get(Constants.PATH_TO_RESOURCES_FOLDER)
-                        .resolve("scenario_test_files/simple-java8-maven-project/pom.xml")
+                        .resolve("scenario_test_files/maven_converter_pom.xml")
                         .toFile();
         org.apache.commons.io.FileUtils.copyFile(
                 pomFile, projectDir.toPath().resolve("pom.xml").toFile());
