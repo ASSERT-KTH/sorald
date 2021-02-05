2847b01735dd1eed5f949e0c6065627e28ea3682
Simon Larsén <slarse@slar.se>
2021-02-05 13:59:11 +0100

    Implement support for legacy records in achievements script

--- experimentation/tools/sorald/achievements.py
+++ experimentation/tools/sorald/achievements.py
@@ -18,8 +18,10 @@ TEMPLATE = r"""# Achievements
 This document presents an overview of the pull requests performed with Sorald.
 {% for pr in pull_requests %}
 ## [{{ pr.repo_slug }}#{{ pr.number }}](https://github.com/{{ pr.repo_slug }}/pull/{{ pr.number }})
-This PR was opened at {{ pr.created_at }}{% if pr.closed_at %} and {{ pr.status }} at {{ pr.closed_at }}{% endif %}.
-{% if pr.contains_manual_edits %}Some manual edits were performed after applying Sorald{% else %}The patch was generated fully automatically with Sorald{% endif %}.
+This PR was opened at {{ pr.created_at }}{% if pr.closed_at %} and {{ pr.status }} at {{ pr.closed_at }}{% endif %}.{% if pr.contains_manual_edits %}
+Some manual edits were performed after applying Sorald.{% elif not pr.is_legacy %}
+The patch was generated fully automatically with Sorald.{% else %}
+This is a legacy PR made before detailed record-keeping, and so we cannot say if any manual edits have been applied.{% endif %}
 {% if pr.repairs|length > 0 %}
 It provide{% if pr.closed_at %}d{% else %}s{% endif %} the following repairs:
 {% for repair in pr.repairs %}
@@ -51,6 +53,7 @@ class PullRequest:
     status: str
     contains_manual_edits: bool
     repairs: List[RepairStats]
+    is_legacy: bool
 
 
 def main(args: List[str]):
@@ -90,13 +93,20 @@ def generate_achievements_file(
     output_file: pathlib.Path,
     template: str,
 ) -> None:
-    pull_requests = parse_pull_requests(prs_json)
+    pull_requests = sorted(
+        parse_pull_requests(prs_json),
+        key=lambda pr: f"{'b' if pr.is_legacy else 'a'}{pr.repo_slug}#{pr.number}",
+    )
     rendered_content = jinja2.Template(template).render(pull_requests=pull_requests)
     output_file.write_text(rendered_content, encoding=ENCODING)
 
 
 def parse_pull_requests(prs_json: pathlib.Path) -> List[PullRequest]:
     prs_data = json.loads(prs_json.read_text(ENCODING))
+
+    def _is_legacy(data: dict) -> bool:
+        return data[jsonkeys.RECORD.SECTION_KEY][jsonkeys.RECORD.IS_LEGACY]
+
     return [
         PullRequest(
             repo_slug=data[jsonkeys.TOP_LEVEL.REPO_SLUG],
@@ -108,24 +118,30 @@ def parse_pull_requests(prs_json: pathlib.Path) -> List[PullRequest]:
             else pr_meta[jsonkeys.PR.STATE],
             contains_manual_edits=len(data[jsonkeys.MANUAL_EDITS.SECTION_KEY] or [])
             > 0,
-            repairs=get_all_repairs(data[jsonkeys.SORALD_STATS.SECTION_KEY]),
+            repairs=get_all_repairs(
+                data[jsonkeys.SORALD_STATS.SECTION_KEY], _is_legacy(data)
+            ),
+            is_legacy=_is_legacy(data),
         )
         for _, data in prs_data.items()
         if (pr_meta := data[jsonkeys.PR.SECTION_KEY])
     ]
 
 
-def get_all_repairs(sorald_stats: dict) -> List[RepairStats]:
-    lst = list(
-        map(
-            parse_repair_stats,
-            sorted(
-                sorald_stats.get(jsonkeys.SORALD_STATS.REPAIRS) or [],
-                key=lambda rep: int(rep[jsonkeys.SORALD_STATS.RULE_KEY]),
-            ),
+def get_all_repairs(sorald_stats: dict, is_legacy: bool) -> List[RepairStats]:
+    return (
+        [parse_legacy_repair_stats(sorald_stats)]
+        if is_legacy
+        else list(
+            map(
+                parse_repair_stats,
+                sorted(
+                    sorald_stats.get(jsonkeys.SORALD_STATS.REPAIRS) or [],
+                    key=lambda rep: int(rep[jsonkeys.SORALD_STATS.RULE_KEY]),
+                ),
+            )
         )
     )
-    return lst
 
 
 def parse_repair_stats(repair_data: dict) -> RepairStats:
@@ -134,6 +150,15 @@ def parse_repair_stats(repair_data: dict) -> RepairStats:
         num_violations_found=repair_data[jsonkeys.SORALD_STATS.VIOLATIONS_BEFORE],
         num_violations_repaired=repair_data[jsonkeys.SORALD_STATS.VIOLATIONS_BEFORE]
         - repair_data[jsonkeys.SORALD_STATS.VIOLATIONS_AFTER],
+    )
+
+
+def parse_legacy_repair_stats(repair_data: dict) -> RepairStats:
+    num_violations = repair_data[jsonkeys.SORALD_STATS.LEGACY.NUM_VIOLATIONS]
+    return RepairStats(
+        rule_key=int(repair_data[jsonkeys.SORALD_STATS.LEGACY.RULE_KEY]),
+        num_violations_found=num_violations,
+        num_violations_repaired=num_violations,
     )
 
 
