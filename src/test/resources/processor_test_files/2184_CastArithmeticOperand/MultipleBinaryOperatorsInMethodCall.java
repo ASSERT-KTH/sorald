c0972a4a303c316815825891f2b8655f4f4e3c44
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-05 15:17:19 +0100

    Apply patch written by @slarse

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/MultipleBinaryOperatorsInMethodCall.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/MultipleBinaryOperatorsInMethodCall.java
@@ -3,6 +3,9 @@ is being analyzed, we don't find it directly in the list of arguments of the met
 is part of something more. Not finding it in the list of arguments causes an IndexOutOfBoundsException because
 we have index -1. This test checks that that exception doesn't happen.
  */
+import java.util.Date;
+
 class MultipleBinaryOperatorsInMethodCall {
+    int seconds = 1000;
     Date myDate = new Date(seconds * 1000 * 10); // Noncompliant
 }
