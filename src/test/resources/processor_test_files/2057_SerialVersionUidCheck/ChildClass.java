946ec699fc94504c905131eb3bb1986ea057b0b1
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-02-05 15:40:27 +0100

    remove package identifier

--- src/test/resources/processor_test_files/2057_SerialVersionUidCheck/ChildClass.java
+++ src/test/resources/processor_test_files/2057_SerialVersionUidCheck/ChildClass.java
@@ -1,5 +1,3 @@
-package processor_test_files.2057_SerialVersionUidCheck;
-
 public class ChildClass extends UpperClass {
   
 }
--- src/test/resources/processor_test_files/2057_SerialVersionUidCheck/UpperClass.java
+++ src/test/resources/processor_test_files/2057_SerialVersionUidCheck/UpperClass.java
@@ -1,5 +1,3 @@
-package processor_test_files.2057_SerialVersionUidCheck;
-
 import java.io.Serializable;
 
 public class UpperClass implements Serializable {
