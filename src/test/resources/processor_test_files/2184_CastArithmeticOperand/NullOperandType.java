67f0c9b2a3165e81427a917e6d1690809923e252
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-08 18:23:36 +0100

    Add a description in the failing test case

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/NullOperandType.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NullOperandType.java
@@ -1,3 +1,7 @@
+/*
+When we cannot resolve the type of the left or right hand operands in a binary operator, we get a NullPointerException.
+This test checks that such an exception doesn't happen.
+*/
 class NullOperandType {
     float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
     String var = response.statusCode() + " ";
