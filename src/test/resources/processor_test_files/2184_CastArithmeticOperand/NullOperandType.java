e15c49d3012689d803fc29277a4741234cafa932
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-08 18:45:35 +0100

    Try with another failing test case

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/NullOperandType.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NullOperandType.java
@@ -4,5 +4,7 @@ This test checks that such an exception doesn't happen.
 */
 class NullOperandType {
     float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
-    String var = response.statusCode() + " ";
+
+    Message message;
+    long var = System.currentTimeMillis() - message.getJMSTimestamp();
 }
