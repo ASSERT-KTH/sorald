5dc88cb2bcfa65e480e315d69f105422fb84e74a
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-08 18:12:17 +0100

    Add a failing test case first (the build must break)

---
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NullOperandType.java
@@ -0,0 +1,4 @@
+class NullOperandType {
+    float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
+    String var = response.statusCode() + " ";
+}
