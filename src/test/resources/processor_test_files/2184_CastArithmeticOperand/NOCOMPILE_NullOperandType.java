1b9b51a401e48633171eb7f4f34cb1d832f57314
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-09 09:44:58 +0100

    Add the prefix NOCOMPILE_ in the test file

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
@@ -4,7 +4,7 @@ This test checks that such an exception doesn't happen.
 */
 import javax.jms.Message;
 
-class NullOperandType {
+class NOCOMPILE_NullOperandType {
     float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
 
     Message message;
