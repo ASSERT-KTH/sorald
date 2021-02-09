c3935b732761fa59936fe0163fc95b7f369ca917
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-09 10:28:13 +0100

    Improve test case as suggested by @slarse

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
@@ -4,5 +4,6 @@ This test checks that such an exception doesn't happen.
 */
 class NOCOMPILE_NullOperandType {
     float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
-    long var = System.currentTimeMillis() - message.getJMSTimestamp();
+    long rhsUnknown = System.currentTimeMillis() - message.getJMSTimestamp();
+    long lhsUnknown = message.getJMSTimestamp() - System.currentTimeMillis();
 }
