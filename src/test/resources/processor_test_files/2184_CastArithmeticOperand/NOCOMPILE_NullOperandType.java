e5ed539e541a91903ad9eba54ab0908253842aa0
Fer Madeiral <fer.madeiral@gmail.com>
2021-02-09 09:53:00 +0100

    Minimize test

--- src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
+++ src/test/resources/processor_test_files/2184_CastArithmeticOperand/NOCOMPILE_NullOperandType.java
@@ -2,11 +2,7 @@
 When we cannot resolve the type of the left or right hand operands in a binary operator, we get a NullPointerException.
 This test checks that such an exception doesn't happen.
 */
-import javax.jms.Message;
-
 class NOCOMPILE_NullOperandType {
     float twoThirds = 2/3; // Noncompliant; THIS LINE WAS ONLY ADDED TO MAKE THE TEST FIND A VIOLATION IN THIS FILE SO THE NEXT LINE CAN BE ACTUALLY TESTED!
-
-    Message message;
     long var = System.currentTimeMillis() - message.getJMSTimestamp();
 }
