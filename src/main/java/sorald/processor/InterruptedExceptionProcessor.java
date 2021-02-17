5e64028cd82336b34a68c60d314ce1c57b4bf276
Simon Larsén <slarse@slar.se>
2021-02-17 11:01:11 +0100

    feat(2142): Add thread interrupt before first throw or return (#385)
    * Add expected output file for InterruptedExceptionProcessor
    
    * Add interrupt as first statement in catch
    
    * Adjust expected output
    
    * Search for the last safe insertion index for the interrupt
    
    * Add new test cases for nested exits
    
    * Add note in HANDLED_RULES about placing the interrupt as late as possible

--- src/main/java/sorald/processor/InterruptedExceptionProcessor.java
+++ src/main/java/sorald/processor/InterruptedExceptionProcessor.java
@@ -1,8 +1,12 @@
 package sorald.processor;
 
 import sorald.annotations.ProcessorAnnotation;
+import spoon.reflect.code.CtBlock;
 import spoon.reflect.code.CtCatch;
 import spoon.reflect.code.CtInvocation;
+import spoon.reflect.code.CtReturn;
+import spoon.reflect.code.CtStatement;
+import spoon.reflect.code.CtThrow;
 import spoon.reflect.code.CtTypeAccess;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtMethod;
@@ -23,11 +27,39 @@ public class InterruptedExceptionProcessor extends SoraldAbstractProcessor<CtCat
         CtTypeAccess<?> threadClassAccess = factory.createTypeAccess(threadClass.getReference());
         CtMethod<?> currentThreadMethod = threadClass.getMethodsByName("currentThread").get(0);
         CtMethod<?> interruptMethod = threadClass.getMethodsByName("interrupt").get(0);
-        CtInvocation firstInvocation =
+        CtInvocation<?> firstInvocation =
                 factory.createInvocation(threadClassAccess, currentThreadMethod.getReference());
-        CtInvocation secondInvocation =
+        CtInvocation<?> secondInvocation =
                 factory.createInvocation(firstInvocation, interruptMethod.getReference());
 
-        element.getBody().addStatement(element.getBody().getStatements().size(), secondInvocation);
+        element.getBody().addStatement(lastSafeInterruptIndex(element.getBody()), secondInvocation);
+    }
+
+    /**
+     * Return the last index that is safe to insert at, safe meaning that the interrupt is certain
+     * to actually be invoked.
+     *
+     * <p>This is either the first index at which a return statement or throw is found, or the index
+     * past the last index if no returns or throws are found.
+     */
+    private static int lastSafeInterruptIndex(CtBlock<?> block) {
+        for (int i = 0; i < block.getStatements().size(); i++) {
+            if (containsReturnOrThrow(block.getStatement(i))) {
+                return i;
+            }
+        }
+        return block.getStatements().size();
+    }
+
+    /**
+     * Check if the given statement contains a return or throw.
+     *
+     * <p>TODO recursively check method declarations if there are method calls.
+     */
+    private static boolean containsReturnOrThrow(CtStatement statement) {
+        return !statement
+                .filterChildren(e -> e instanceof CtReturn || e instanceof CtThrow)
+                .list()
+                .isEmpty();
     }
 }
