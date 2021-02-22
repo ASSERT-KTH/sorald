453a440a87b649483a0596e5d0f30761ce4a9208
Martin Wittlinger <wittlinger.martin@gmail.com>
2021-02-22 01:38:59 +0000

    fix nullptr

--- src/main/java/sorald/processor/SerialVersionUidProcessor.java
+++ src/main/java/sorald/processor/SerialVersionUidProcessor.java
@@ -90,7 +90,13 @@ public class SerialVersionUidProcessor extends SoraldAbstractProcessor<CtClass<?
         while (currentClass != null) {
             superTypes.addAll(currentClass.getSuperInterfaces());
             superTypes.add(currentClass.getSuperclass());
-            currentClass = currentClass.getSuperclass().getTypeDeclaration();
+            if(currentClass.getSuperclass() != null) {
+                currentClass = currentClass.getSuperclass().getTypeDeclaration();
+            }
+            else {
+                currentClass = null;
+            }
+            
         }
         superTypes.removeIf(Objects::isNull);
         for (CtTypeReference<?> ctTypeReference : superTypes) {
