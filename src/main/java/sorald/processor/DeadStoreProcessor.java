d8380a882f1fc250183b4978dee6b134ba823266
Simon Larsén <slarse@slar.se>
2021-02-23 13:34:51 +0100

    Implement first working prototype

--- src/main/java/sorald/processor/DeadStoreProcessor.java
+++ src/main/java/sorald/processor/DeadStoreProcessor.java
@@ -1,6 +1,10 @@
 package sorald.processor;
 
+import java.util.IdentityHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import sorald.annotations.ProcessorAnnotation;
 import spoon.reflect.code.CtAssignment;
 import spoon.reflect.code.CtLocalVariable;
@@ -39,17 +43,93 @@ public class DeadStoreProcessor extends SoraldAbstractProcessor<CtStatement> {
         if (varAccesses.isEmpty()) {
             localVar.delete();
         } else {
-            varAccesses.stream()
-                    .filter(access -> access instanceof CtVariableWrite)
-                    .map(write -> (CtAssignment) write.getParent())
-                    .findFirst()
-                    .ifPresent(
-                            assignment -> {
-                                localVar.setAssignment(assignment.getAssignment());
-                                localVar.delete();
-                                assignment.replace(localVar.clone());
-                            });
+            List<CtStatementList> varAccessStatementLists =
+                    varAccesses.stream()
+                            .map(access -> access.getParent(CtStatementList.class))
+                            .distinct() // TODO optimize, this will use very slow equality
+                            // comparison
+                            .collect(Collectors.toList());
+            Map<CtElement, Integer> statementListDepths =
+                    computeDepths(
+                            statementList,
+                            statementList.getElements(e -> e instanceof CtStatementList));
+
+            CtStatementList commonParent =
+                    findCommonParentList(varAccessStatementLists, statementListDepths);
+            Optional<CtVariableAccess<?>> firstVarAccessInCommonParent =
+                    varAccesses.stream()
+                            .filter(
+                                    access ->
+                                            access.getParent(CtStatementList.class) == commonParent)
+                            .findFirst();
+            int indexOfFirstAccess = -1;
+            for (int i = 0; i < commonParent.getStatements().size(); i++) {
+                var statement = commonParent.getStatement(i);
+                if (!statement.getElements(accessFilter(localVar)).isEmpty()) {
+                    indexOfFirstAccess = i;
+                    break;
+                }
+            }
+
+            if (firstVarAccessInCommonParent.isPresent()
+                    && firstVarAccessInCommonParent.get() instanceof CtVariableWrite
+                    && commonParent
+                                    .getStatements()
+                                    .indexOf(
+                                            firstVarAccessInCommonParent
+                                                    .get()
+                                                    .getParent(CtStatement.class))
+                            == indexOfFirstAccess) {
+                CtVariableWrite<?> write = (CtVariableWrite<?>) firstVarAccessInCommonParent.get();
+                moveDeclarationToWrite(write, localVar);
+            } else {
+                commonParent.addStatement(localVar.clone());
+            }
+
+            localVar.delete();
         }
+    }
+
+    private void moveDeclarationToWrite(CtVariableWrite<?> write, CtLocalVariable<?> localVar) {
+        CtAssignment assignment = write.getParent(CtAssignment.class);
+        localVar.setAssignment(assignment.getAssignment());
+        assignment.replace(localVar.clone());
+    }
+
+    private CtStatementList findCommonParentList(
+            List<CtStatementList> statementLists, Map<CtElement, Integer> depths) {
+        if (statementLists.size() == 1) {
+            return statementLists.get(0);
+        }
+
+        return statementLists.stream()
+                .reduce((lhs, rhs) -> findCommonParentList(lhs, rhs, depths))
+                .get();
+    }
+
+    private CtStatementList findCommonParentList(
+            CtStatementList lhs, CtStatementList rhs, Map<CtElement, Integer> depths) {
+        while (true) {
+            if (lhs == rhs) {
+                return lhs;
+            } else if (depths.get(lhs).equals(depths.get(rhs))) {
+                lhs = lhs.getParent(CtStatementList.class);
+                rhs = rhs.getParent(CtStatementList.class);
+            }
+            while (depths.get(lhs) > depths.get(rhs)) {
+                lhs = lhs.getParent(CtStatementList.class);
+            }
+            while (depths.get(rhs) > depths.get(lhs)) {
+                rhs = rhs.getParent(CtStatementList.class);
+            }
+        }
+    }
+
+    private Map<CtElement, Integer> computeDepths(
+            CtElement parent, List<? extends CtElement> children) {
+        Map<CtElement, Integer> idMap = new IdentityHashMap<>();
+        children.forEach(child -> idMap.putIfAbsent(child, depth(child, parent)));
+        return idMap;
     }
 
     private int depth(CtElement child, CtElement parent) {
