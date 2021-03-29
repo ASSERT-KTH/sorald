635d03fa72b120552e97e9c5f4fb01c7ac84390e
Simon Larsén <slarse@slar.se>
2021-03-29 14:13:43 +0200

    Add test case

---
+++ src/test/resources/processor_test_files/2142_InterruptedException/CatchMultipleTypes.java
@@ -0,0 +1,20 @@
+import java.util.concurrent.ExecutionException;
+
+/**
+ We should only set the interrupted flag if an interrupt actually occurred, so when a catch has
+ multiple types in the catch clause, we need to do a type check.
+ */
+
+public class CatchMultipleTypes {
+    public static void method() {
+        try {
+            if (1 < 2) {
+                throw new ExecutionException(new RuntimeException());
+            } else {
+                throw new InterruptedException();
+            }
+        } catch (InterruptedException | ExecutionException e) { // Noncompliant
+            throw new RuntimeException(e);
+        }
+    }
+}
