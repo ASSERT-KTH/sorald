6421d0cd5f727de313e375ece627cb968aac918a
Simon Larsén <slarse@slar.se>
2021-03-29 17:25:22 +0200

    Implement first version of processor

--- src/main/java/sorald/processor/UtilityClassWithPublicConstructorProcessor.java
+++ src/main/java/sorald/processor/UtilityClassWithPublicConstructorProcessor.java
@@ -1,8 +1,12 @@
 package sorald.processor;
 
+import java.util.List;
+import java.util.Set;
 import sorald.annotations.IncompleteProcessor;
 import sorald.annotations.ProcessorAnnotation;
 import spoon.reflect.declaration.CtClass;
+import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.ModifierKind;
 
 @IncompleteProcessor(description = "Only handles implicit public constructor")
 @ProcessorAnnotation(
@@ -11,5 +15,20 @@ import spoon.reflect.declaration.CtClass;
 public class UtilityClassWithPublicConstructorProcessor
         extends SoraldAbstractProcessor<CtClass<?>> {
     @Override
-    protected void repairInternal(CtClass<?> element) {}
+    protected void repairInternal(CtClass<?> element) {
+        addExplicitConstructor(element);
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <T> void addExplicitConstructor(CtClass<T> cls) {
+        CtConstructor<T> explicitConstructor =
+                cls.getFactory()
+                        .createConstructor(
+                                cls,
+                                Set.of(ModifierKind.PRIVATE),
+                                List.of(),
+                                Set.of(),
+                                cls.getFactory().createBlock());
+        cls.getConstructor().replace(explicitConstructor);
+    }
 }
