688081a6d17658972413060e0eff9e070785f2a3
Simon Larsén <slarse@slar.se>
2021-03-30 14:04:32 +0200

    Only repair in the absence of explicit constructors

--- src/main/java/sorald/processor/UtilityClassWithPublicConstructorProcessor.java
+++ src/main/java/sorald/processor/UtilityClassWithPublicConstructorProcessor.java
@@ -6,6 +6,7 @@ import sorald.annotations.IncompleteProcessor;
 import sorald.annotations.ProcessorAnnotation;
 import spoon.reflect.declaration.CtClass;
 import spoon.reflect.declaration.CtConstructor;
+import spoon.reflect.declaration.CtElement;
 import spoon.reflect.declaration.ModifierKind;
 
 @IncompleteProcessor(description = "Only handles implicit public constructor")
@@ -14,6 +15,12 @@ import spoon.reflect.declaration.ModifierKind;
         description = "Utility classes should not have public constructors")
 public class UtilityClassWithPublicConstructorProcessor
         extends SoraldAbstractProcessor<CtClass<?>> {
+
+    @Override
+    protected boolean canRepairInternal(CtClass<?> candidate) {
+        return candidate.getConstructors().stream().allMatch(CtElement::isImplicit);
+    }
+
     @Override
     protected void repairInternal(CtClass<?> element) {
         addExplicitConstructor(element);
