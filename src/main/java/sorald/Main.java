f88c94e4697bbfb40f782a3c6365861db3db0d71
Simon Larsén <slarse@slar.se>
2021-05-17 15:31:46 +0200

    Add nonsensical rule violation

--- src/main/java/sorald/Main.java
+++ src/main/java/sorald/Main.java
@@ -4,6 +4,12 @@ import sorald.cli.Cli;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            throw new InterruptedException();
+        } catch (InterruptedException | IllegalArgumentException e) {
+            System.out.println("oops!");
+        }
+
         int exitStatus = Cli.createCli().execute(args);
         if (exitStatus != 0) {
             System.exit(exitStatus);
