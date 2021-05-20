beb5748f87c7d28687ca16140571f3c3c0a7d0c8
Simon Larsén <slarse@slar.se>
2021-05-20 09:05:33 +0200

    Revert "Use default fetch-depth"
    This reverts commit 7805995f4ea0313b33d7be440a555cba0dc9c7c1.

--- src/main/java/sorald/Main.java
+++ src/main/java/sorald/Main.java
@@ -8,9 +8,6 @@ public class Main {
             throw new InterruptedException();
         } catch (InterruptedException | IllegalArgumentException e) {
             System.out.println("oops!");
-            if (e instanceof InterruptedException) {
-                Thread.currentThread().interrupt();
-            }
         }
 
         int exitStatus = Cli.createCli().execute(args);
