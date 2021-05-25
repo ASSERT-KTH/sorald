ebbfc783dc191bfba4fa9634bb1a84e8c8514631
Simon Larsén <slarse@slar.se>
2021-05-25 16:30:21 +0200

    Implement naive version of processor

--- src/main/java/sorald/processor/EqualsArgumentTypeProcessor.java
+++ src/main/java/sorald/processor/EqualsArgumentTypeProcessor.java
@@ -1,11 +1,21 @@
 package sorald.processor;
 
 import sorald.annotations.ProcessorAnnotation;
+import spoon.reflect.code.CtStatement;
 import spoon.reflect.declaration.CtMethod;
 
 @ProcessorAnnotation(key = 2097, description = "\"equals(Object obj)\" should test argument type")
 public class EqualsArgumentTypeProcessor extends SoraldAbstractProcessor<CtMethod<?>> {
 
     @Override
-    protected void repairInternal(CtMethod<?> element) {}
+    protected void repairInternal(CtMethod<?> element) {
+        String paramName = element.getParameters().get(0).getSimpleName();
+        CtStatement typeTest =
+                element.getFactory()
+                        .createCodeSnippetStatement(
+                                String.format(
+                                        "if (%s == null || getClass() != %s.getClass()) { return false; }",
+                                        paramName, paramName));
+        element.getBody().addStatement(0, typeTest);
+    }
 }
