024ddd2a6336ea65386f04c37554df1310861a38
Simon Larsén <slarse@slar.se>
2021-06-09 16:29:07 +0200

    Add test case

---
+++ src/test/resources/processor_test_files/2095_UnclosedResources/BadCloseInFinally.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BadCloseInFinally {
+
+    public static void saveTo(File file1, File file2) {
+        BufferedWriter bw1 = null;
+        BufferedWriter bw2 = null;
+        try {
+            bw1 = new BufferedWriter(new FileWriter(file1));
+            bw1.write("Write some stuff to file 1");
+
+            bw2 = new BufferedWriter(new FileWriter(file2)); // Noncompliant
+            bw2.write("Write some stuff to file 2");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("done");
+            try {
+                if (bw1 != null) {
+                    bw1.close();
+                }
+                if (bw2 != null) {
+                    bw2.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
