ce088b6b6869c95b3f07690aaace32791e928b64
Simon Larsén <slarse@slar.se>
2021-06-10 16:24:47 +0200

    Add test case with field with same name as local variable
    This is to ensure that we don't accidentally remove references to the field

---
+++ src/test/resources/processor_test_files/2095_UnclosedResources/ReferenceInCatcherAndFinalizerToFieldWithSameNameAsResource.java
@@ -0,0 +1,32 @@
+/*
+When the processor inlines a local variable declaration into the resources block, it also cleans
+up referenses to said variable in the catchers and finalizer. This test case includes a field
+with the same name as the inlined variable, and is meant to verify that we don't accidentally
+delete references to this unrelated field.
+ */
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class ReferenceInCatcherAndFinalizerToFieldWithSameNameAsResource {
+    private int bw = 2;
+
+    public void saveTo(File file) {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter(file)); // Noncompliant
+            bw.write("Write some stuff to file");
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println(this.bw); // reference to field; should not be cleaned!
+            if (bw != null) { // reference to local variable; should be cleaned!
+                bw.close();
+            }
+        } finally {
+            System.out.println(this.bw); // reference to field; should not be cleaned!
+            System.out.println("done");
+        }
+    }
+}
