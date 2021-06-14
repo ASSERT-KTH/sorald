08936e4dbfa24bf329e474a3ddc0cd03054607d2
Simon Larsén <slarse@slar.se>
2021-06-14 10:13:34 +0200

    test: Verify that processor test resources compile (#566)
    * Add test for checking that processor test input source files compile
    
    * Add test case to verify that expected files compile
    
    * Fix broken test resource that was accidentally committed
    
    * Refactor assertCompile

---
+++ src/test/java/sorald/processor/ProcessorTestFilesCompileTest.java
@@ -0,0 +1,104 @@
+package sorald.processor;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import javax.tools.Diagnostic;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaFileObject;
+import javax.tools.ToolProvider;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+/** Meta tests for verifying that the processor test files compile as expected. */
+class ProcessorTestFilesCompileTest {
+
+    @ParameterizedTest
+    @MethodSource("provideCompilableProcessorTestInputFile")
+    void processorTestCaseInputFile_notMarkedNOCOMPILE_shouldCompile(File testCaseJavaFile)
+            throws IOException {
+        assertCompiles(testCaseJavaFile);
+    }
+
+    private static Stream<Arguments> provideCompilableProcessorTestInputFile() {
+        return getCompilableProcessorTestCases().map(tc -> tc.nonCompliantFile).map(Arguments::of);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideCompilableProcessorTestExpectedFiles")
+    void processorTestCaseExpectedFile_notMarkedNOCOMPILE_shouldCompile(
+            File testCaseExpectedJavaFile, @TempDir File tempDir) throws IOException {
+        File javaFile = copyFileToDirWithoutExpectedExtension(testCaseExpectedJavaFile, tempDir);
+        assertCompiles(javaFile);
+    }
+
+    private static Stream<Arguments> provideCompilableProcessorTestExpectedFiles() {
+        return getCompilableProcessorTestCases()
+                .flatMap(tc -> tc.expectedOutfile().stream())
+                .map(Arguments::of);
+    }
+
+    private static File copyFileToDirWithoutExpectedExtension(File expectedJavaFile, File dir)
+            throws IOException {
+        String fileNameWithExpectedExtension = expectedJavaFile.getName();
+        String validJavaFileName =
+                fileNameWithExpectedExtension.substring(
+                        0, fileNameWithExpectedExtension.lastIndexOf("."));
+        File validJavaFile = dir.toPath().resolve(validJavaFileName).toFile();
+        FileUtils.copyFile(expectedJavaFile, validJavaFile);
+        return validJavaFile;
+    }
+
+    private static Stream<ProcessorTestHelper.ProcessorTestCase<?>>
+            getCompilableProcessorTestCases() {
+        return ProcessorTestHelper.getTestCaseStream(ProcessorTestHelper.TEST_FILES_ROOT.toFile())
+                .filter(
+                        tc ->
+                                ProcessorTestHelper.isStandaloneCompilableTestFile(
+                                        tc.nonCompliantFile));
+    }
+
+    private static void assertCompiles(File javaFile) throws IOException {
+        var compileResults = compile(javaFile);
+        boolean compileSuccessful = compileResults.getLeft();
+        String diagnosticsOutput = compileResults.getRight();
+
+        assertTrue(compileSuccessful, diagnosticsOutput);
+    }
+
+    /** Returns a pair (compileSuccess, diagnosticsOutput) */
+    private static Pair<Boolean, String> compile(File javaFile) {
+        // inspired comment by user GETah on StackOverflow: https://stackoverflow.com/a/8364016
+
+        var compiler = ToolProvider.getSystemJavaCompiler();
+        assertThat(
+                "System does not have a Java compiler, please run test suite with a JDK",
+                compiler,
+                notNullValue());
+
+        var diagnostics = new DiagnosticCollector<JavaFileObject>();
+        var fileManager = compiler.getStandardFileManager(diagnostics, null, null);
+        var compilationUnits =
+                fileManager.getJavaFileObjectsFromStrings(List.of(javaFile.getAbsolutePath()));
+        var task = compiler.getTask(null, fileManager, diagnostics, null, null, compilationUnits);
+
+        boolean success = task.call();
+
+        String diagnosticsOutput =
+                diagnostics.getDiagnostics().stream()
+                        .map(Diagnostic::toString)
+                        .collect(Collectors.joining(System.lineSeparator()));
+
+        return Pair.of(success, diagnosticsOutput);
+    }
+}
