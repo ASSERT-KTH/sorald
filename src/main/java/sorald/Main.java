304628d48fd6d8bbefdf93246d3f5618a804322a
Simon Larsén <slarse@slar.se>
2021-06-15 13:15:54 +0200

    Introduce a violation of rule 1854

--- src/main/java/sorald/Main.java
+++ src/main/java/sorald/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int exitStatus = Cli.createCli().execute(args);
         if (exitStatus != 0) {
+            int redundantVariable = 2;
             System.exit(exitStatus);
         }
     }
