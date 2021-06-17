9757b2592bae0fa6cc778ce6d8eb5bbd08d796cd
Fer Madeiral <fer.madeiral@gmail.com>
2021-06-17 10:13:39 +0200

    Add special case in the test files as suggested by @slarse

--- src/test/resources/processor_test_files/1596_CollectionsEmptyConstants/CollectionsEmptyConstants.java
+++ src/test/resources/processor_test_files/1596_CollectionsEmptyConstants/CollectionsEmptyConstants.java
@@ -8,5 +8,6 @@ public class CollectionsEmptyConstants {
         List<String> collection1 = Collections.EMPTY_LIST;  // Noncompliant
         Map<String, String> collection2 = Collections.EMPTY_MAP;  // Noncompliant
         Set<String> collection3 = Collections.EMPTY_SET;  // Noncompliant
+        List<Integer> list = java.util.Collections.EMPTY_LIST; // Compliant due to false positive
     }
 }
