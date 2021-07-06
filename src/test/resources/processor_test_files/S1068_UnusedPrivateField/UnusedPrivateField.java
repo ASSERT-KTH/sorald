2df3a0423324572ae3d4ea1c3b939c8b745fa71e
Aman Sharma <mannu.poski10@gmail.com>
2021-07-06 19:54:17 +0530

    Add case for private fields which are used in the class

--- src/test/resources/processor_test_files/S1068_UnusedPrivateField/UnusedPrivateField.java
+++ src/test/resources/processor_test_files/S1068_UnusedPrivateField/UnusedPrivateField.java
@@ -1,12 +1,13 @@
 import java.io.Serializable;
 
 public class UnusedPrivateField implements Serializable {
-    private static final long serialVersionUID = 42L; // Compliant as this field is used in deserialization
     private int a = 42; // Noncompliant
     private static String b = "Hello world!"; // Noncompliant
-    protected double SOME_CONSTANT = 3.14; // Compliant as this field may be used in subclasses
+    private static final long serialVersionUID = 42L; // Compliant as this field is used in deserialization
+    protected double c = 3.14; // Compliant as this field may be used in subclasses
+    private static int d = 1; // Compliant as this field is used below
 
     public int compute(int number) {
-        return number * 42;
+        return number * d;
     }
 }
