0e52c355efa1a182774fe564b6a733802df3faf5
Aman Sharma <mannu.poski10@gmail.com>
2021-07-06 20:38:20 +0530

    Add special case for joint declarations

--- src/test/resources/processor_test_files/S1068_UnusedPrivateField/UnusedPrivateField.java
+++ src/test/resources/processor_test_files/S1068_UnusedPrivateField/UnusedPrivateField.java
@@ -5,9 +5,9 @@ public class UnusedPrivateField implements Serializable {
     private static String b = "Hello world!"; // Noncompliant
     private static final long serialVersionUID = 42L; // Compliant as this field is used in deserialization
     protected double c = 3.14; // Compliant as this field may be used in subclasses
-    private static int d = 1; // Compliant as this field is used below
+    private static int used = 1, unused = 2; // Compliant and noncompliant respectively
 
     public int compute(int number) {
-        return number * d;
+        return number * used;
     }
 }
