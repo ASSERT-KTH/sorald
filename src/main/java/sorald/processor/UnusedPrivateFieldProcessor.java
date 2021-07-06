86fbbce6cabb8252c8fe21c3792cb3ab11b82380
Aman Sharma <mannu.poski10@gmail.com>
2021-07-06 17:28:41 +0530

    Add processor for deleting unused `private` fields

---
+++ src/main/java/sorald/processor/UnusedPrivateFieldProcessor.java
@@ -0,0 +1,12 @@
+package sorald.processor;
+
+import sorald.annotations.ProcessorAnnotation;
+import spoon.reflect.declaration.CtField;
+
+@ProcessorAnnotation(key = "S1068", description = "Unused \"private\" fields should be removed")
+public class UnusedPrivateFieldProcessor extends SoraldAbstractProcessor<CtField<?>> {
+    @Override
+    protected void repairInternal(CtField<?> element) {
+        element.delete();
+    }
+}
