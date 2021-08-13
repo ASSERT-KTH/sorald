86dce167a76155de44b5dcc323852eccd882e3e5
Aman Sharma <mannu.poski10@gmail.com>
2021-08-13 19:50:43 +0530

    Refactor fetching of processors

--- src/test/java/sorald/processor/ProcessorDescriptionTest.java
+++ src/test/java/sorald/processor/ProcessorDescriptionTest.java
@@ -5,11 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.io.FilenameUtils;
 import org.junit.jupiter.api.Test;
+import sorald.Processors;
 
 public class ProcessorDescriptionTest {
     private static final Path PROCESSOR_PACKAGE = Paths.get("src/main/java/sorald/processor");
@@ -17,19 +17,18 @@ public class ProcessorDescriptionTest {
     @Test
     public void test_eachProcessorIsAccompaniedByDescription() {
         List<File> processors =
-                Arrays.stream(PROCESSOR_PACKAGE.toFile().listFiles())
-                        .filter(file -> file.getName().endsWith(".java"))
+                Processors.getAllProcessors().stream()
+                        .map(Class::getSimpleName)
+                        .map(procName -> PROCESSOR_PACKAGE.resolve(procName + ".java"))
+                        .map(Path::toFile)
                         .collect(Collectors.toList());
         processors.forEach(
-                processor -> {
-                    if (processor.getName().equals("SoraldAbstractProcessor.java")) return;
-
-                    assertTrue(
-                            getDescription(processor).isFile(),
-                            "Description corresponding to "
-                                    + processor.getName()
-                                    + " does not exist.");
-                });
+                processor ->
+                        assertTrue(
+                                getDescription(processor).isFile(),
+                                "Description corresponding to "
+                                        + processor.getName()
+                                        + " does not exist."));
     }
 
     private static File getDescription(File processor) {
