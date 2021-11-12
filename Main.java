9a57e6693fdca3d0ee4a950cb89a7afbffed31f4
Simon Larsén <slarse@slar.se>
2021-11-12 19:36:49 +0100

    Try using full path

--- Main.java
+++ Main.java
@@ -1,26 +1,31 @@
 
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         String[] mvnVersion = {"mvn", "--version"};
         String[] mvnCmdVersion = {"mvn.cmd", "-version"};
+        String[] mvnAbsPathVersion = {"C:\\ProgramData\\chocolatey\\lib\\maven\\apache-maven-3.8.3\\bin\\mvn.cmd", "-version"};
 
         runProcess(mvnVersion);
         runProcess(mvnCmdVersion);
+        runProcess(mvnAbsPathVersion);
     }
 
-    public static void runProcess(String[] cmd) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder(cmd);
-        // No STDERR => merge to STDOUT
-        pb.redirectErrorStream(true);
+    public static void runProcess(String[] cmd) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(cmd);
+            // No STDERR => merge to STDOUT
+            pb.redirectErrorStream(true);
 
-        Process p = pb.start();
+            Process p = pb.start();
 
+            try(var stdo = p.getInputStream()) {
+                stdo.transferTo(System.out);
+            }
 
-        try(var stdo = p.getInputStream()) {
-            stdo.transferTo(System.out);
+            p.waitFor();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        p.waitFor();
     }
 }
