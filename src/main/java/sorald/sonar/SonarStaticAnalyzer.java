0b42f6f249ad059166939e4758ef55a48303c9af
Aman Sharma <mannu.poski10@gmail.com>
2022-02-01 08:51:55 +0100

    Filter out issues with null text range

--- src/main/java/sorald/sonar/SonarStaticAnalyzer.java
+++ src/main/java/sorald/sonar/SonarStaticAnalyzer.java
@@ -55,7 +55,10 @@ public class SonarStaticAnalyzer implements StaticAnalyzer {
         var issueHandler = new IssueHandler();
         sonarLint.analyze(config, issueHandler, null, null);
         sonarLint.stop();
-        return issueHandler.issues.stream().map(ScannedViolation::new).collect(Collectors.toList());
+        return issueHandler.issues.stream()
+                .filter(issue -> issue.getTextRange() != null)
+                .map(ScannedViolation::new)
+                .collect(Collectors.toList());
     }
 
     private static class IssueHandler implements IssueListener {
