9a7bc9c90eea940432fac923c570dc37d06c27e9
Aman Sharma <mannu.poski10@gmail.com>
2022-05-20 14:04:53 +0200

    refactor: reuse APIs defined in RuleRepository in RuleProvider (#812)
    * Use APIs defined in RuleRepository in RuleProvider
    
    * Use valid syntax to document return value of RuleProvider.inferRules
    
    * Test all cases defined in RuleProvider.inferRules
    
    * Rename test class to match the API definition

--- sorald/src/test/java/sorald/sonar/SonarRulesTest.java
+++ sorald/src/test/java/sorald/sonar/SonarRulesTest.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import sorald.Processors;
 import sorald.rule.IRuleType;
@@ -21,31 +22,51 @@ import sorald.rule.Rule;
 import sorald.rule.RuleProvider;
 
 class SonarRulesTest {
-    @Test
-    void getRulesByType_subsetOfRulesShouldHaveCorrectRuleType() {
-        // arrange
-        List<IRuleType> ruleTypes = List.of(SonarRuleType.VULNERABILITY);
+    @Nested
+    class InferRules {
+        @Test
+        void shouldHaveAllHandledRules() {
+            // arrange
+            List<IRuleType> ruleTypes = List.of();
+            boolean handledRules = true;
 
-        // act
-        Collection<Rule> rules = RuleProvider.getRulesByType(ruleTypes);
+            // act
+            Collection<Rule> rules = RuleProvider.inferRules(ruleTypes, handledRules);
 
-        // assert
-        rules.forEach(rule -> assertThat(rule.getType(), equalTo(SonarRuleType.VULNERABILITY)));
-    }
+            // assert
+            assertThat(rules.size(), equalTo(Processors.getAllProcessors().size()));
+            rules.forEach(
+                    rule -> assertThat(Processors.getProcessor(rule.getKey()), is(notNullValue())));
+        }
 
-    @Test
-    void inferRules_subsetOfRulesShouldHaveACorrespondingProcessor() {
-        // arrange
-        List<IRuleType> ruleTypes = List.of();
-        boolean handledRules = true;
+        @Test
+        void shouldHaveAllRulesFilteredByRuleTypes() {
+            // arrange
+            List<IRuleType> ruleTypes = List.of(SonarRuleType.VULNERABILITY);
 
-        // act
-        Collection<Rule> rules = RuleProvider.inferRules(ruleTypes, handledRules);
+            // act
+            Collection<Rule> rules = RuleProvider.getRulesByType(ruleTypes);
 
-        // assert
-        assertThat(rules.size(), equalTo(Processors.getAllProcessors().size()));
-        rules.forEach(
-                rule -> assertThat(Processors.getProcessor(rule.getKey()), is(notNullValue())));
+            // assert
+            rules.forEach(rule -> assertThat(rule.getType(), equalTo(SonarRuleType.VULNERABILITY)));
+        }
+
+        @Test
+        void shouldHaveAllHandledRulesFilteredByRuleTypes() {
+            // arrange
+            List<IRuleType> ruleTypes = List.of(SonarRuleType.CODE_SMELL);
+            boolean handledRules = true;
+
+            // act
+            Collection<Rule> rules = RuleProvider.inferRules(ruleTypes, handledRules);
+
+            // assert
+            rules.forEach(
+                    rule -> {
+                        assertThat(Processors.getProcessor(rule.getKey()), is(notNullValue()));
+                        assertThat(rule.getType(), is(SonarRuleType.CODE_SMELL));
+                    });
+        }
     }
 
     /**
