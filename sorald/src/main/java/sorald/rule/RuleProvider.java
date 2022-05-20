1fb189a865971a86d6ac9d9b32c993e54f3c214f
Aman Sharma <mannu.poski10@gmail.com>
2022-05-20 13:28:31 +0200

    Use APIs defined in RuleRepository in RuleProvider

--- sorald/src/main/java/sorald/rule/RuleProvider.java
+++ sorald/src/main/java/sorald/rule/RuleProvider.java
@@ -1,12 +1,11 @@
 package sorald.rule;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.ServiceLoader.Provider;
-import java.util.Set;
 import java.util.stream.Collectors;
-import sorald.Processors;
 import sorald.api.RuleRepository;
 
 /** Entrypoint for finding available rules. */
@@ -34,9 +33,10 @@ public class RuleProvider {
      * @return All rules with any of the given types.
      */
     public static Collection<Rule> getRulesByType(IRuleType... types) {
-        var ruleTypes = Set.of(types);
-        return getAllRules().stream()
-                .filter(rule -> ruleTypes.contains(rule.getType()))
+        return ServiceLoader.load(RuleRepository.class).stream()
+                .map(Provider::get)
+                .map(rr -> rr.getRulesByType(types))
+                .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
 
@@ -51,20 +51,64 @@ public class RuleProvider {
     }
 
     /**
-     * Infer which rules to use based on rule types specified (or left unspecified) on the command
-     * line.
+     * Returns a collection of rules that are handled by Sorald.
+     *
+     * @return Handled rules.
+     */
+    public static Collection<Rule> getHandledRules() {
+        return ServiceLoader.load(RuleRepository.class).stream()
+                .map(Provider::get)
+                .map(RuleRepository::getHandledRules)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Get all handled rules matching one of the given types.
+     *
+     * @param types Types to filter rules by.
+     * @return Handled rules with any of the given types.
+     */
+    public static Collection<Rule> getHandledRulesByType(IRuleType... types) {
+        return ServiceLoader.load(RuleRepository.class).stream()
+                .map(Provider::get)
+                .map(rr -> rr.getHandledRulesByType(types))
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Get all handled rules matching one of the given types.
+     *
+     * @param types Types to filter rules by.
+     * @return Handled rules with any of the given types.
+     */
+    public static Collection<Rule> getHandledRulesByType(Collection<IRuleType> types) {
+        return getHandledRulesByType(types.toArray(IRuleType[]::new));
+    }
+
+    /**
+     * Infer which rules to use based on rule types specified (or left unspecified) and their repair
+     * status by Sorald on the command line.
+     *
+     * @param ruleTypes Types to filter rules by.
+     * @param handledRules whether to fetch only handled rules.
+     * @return Subset of {@link RuleRepository##getAllRules()}.
      */
     public static List<Rule> inferRules(List<IRuleType> ruleTypes, boolean handledRules) {
-        List<Rule> rules =
-                List.copyOf(
-                        ruleTypes.isEmpty()
-                                ? RuleProvider.getAllRules()
-                                : RuleProvider.getRulesByType(ruleTypes));
-
-        return !handledRules
-                ? rules
-                : rules.stream()
-                        .filter(rule -> Processors.getProcessor(rule.getKey()) != null)
-                        .collect(Collectors.toList());
+        if (ruleTypes.isEmpty() && !handledRules) {
+            return new ArrayList<>(getAllRules());
+        }
+        // `handledRules` is redundant here, but I am leaving it for the sake of
+        // readability.
+        if (ruleTypes.isEmpty() && handledRules) {
+            return new ArrayList<>(getHandledRules());
+        }
+        // `ruleTypes` is redundant here, but I am leaving it for the sake of
+        // readability.
+        if (!ruleTypes.isEmpty() && !handledRules) {
+            return new ArrayList<>(getRulesByType(ruleTypes));
+        }
+        return new ArrayList<>(getHandledRulesByType(ruleTypes));
     }
 }
