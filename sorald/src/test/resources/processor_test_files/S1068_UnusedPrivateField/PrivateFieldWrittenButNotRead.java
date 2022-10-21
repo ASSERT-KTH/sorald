76e4015a50a4819f244b30a10af17d6cf0e6832a
Aman Sharma <mannu.poski10@gmail.com>
2022-10-21 14:41:18 +0200

    Modify tests

--- sorald/src/test/resources/processor_test_files/S1068_UnusedPrivateField/PrivateFieldWrittenButNotRead.java
+++ sorald/src/test/resources/processor_test_files/S1068_UnusedPrivateField/PrivateFieldWrittenButNotRead.java
@@ -3,11 +3,18 @@ public class PrivateFieldWrittenButNotRead {
 
     private String foo; // Noncompliant
 
+    private long used;
+
     public PrivateFieldWrittenButNotRead(int x, int y) {
         this.x = x + y;
     }
 
     public void initialize(String bar) {
         foo = bar;
+        used = 234324L;
+    }
+
+    public long getUsed() {
+        return used;
     }
 }
