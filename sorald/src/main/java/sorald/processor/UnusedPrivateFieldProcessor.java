ded7b031301b14657b5f9c9d09a86cd3a92cda30
Aman Sharma <mannu.poski10@gmail.com>
2022-10-21 14:42:45 +0200

    fix(S1068): delete field writes of corresponding field only

--- sorald/src/main/java/sorald/processor/UnusedPrivateFieldProcessor.java
+++ sorald/src/main/java/sorald/processor/UnusedPrivateFieldProcessor.java
@@ -1,6 +1,7 @@
 package sorald.processor;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import sorald.annotations.ProcessorAnnotation;
 import spoon.reflect.code.CtFieldWrite;
 import spoon.reflect.code.CtStatement;
@@ -12,18 +13,22 @@ import spoon.reflect.visitor.filter.TypeFilter;
 public class UnusedPrivateFieldProcessor extends SoraldAbstractProcessor<CtField<?>> {
     @Override
     protected void repairInternal(CtField<?> element) {
-        removeAllUnusedPrivateFieldWrites(element);
-        element.delete();
-    }
+        List<CtFieldWrite> unusedFieldWrites = getFieldWritesOfSpecifiedField(element);
 
-    /** Removes all writes to the given field that are not read anywhere. */
-    private static void removeAllUnusedPrivateFieldWrites(CtField<?> field) {
-        CtClass<?> parentClass = field.getParent(CtClass.class);
-        List<CtFieldWrite<?>> unusedFieldWrites =
-                parentClass.filterChildren(new TypeFilter<>(CtFieldWrite.class)).list();
         for (CtFieldWrite<?> unusedFieldWrite : unusedFieldWrites) {
             CtStatement statement = unusedFieldWrite.getParent(CtStatement.class);
             statement.delete();
         }
+        element.delete();
+    }
+
+    private static List<CtFieldWrite> getFieldWritesOfSpecifiedField(CtField<?> field) {
+        CtClass<?> parentClass = field.getParent(CtClass.class);
+        return parentClass
+                .filterChildren(new TypeFilter<>(CtFieldWrite.class))
+                .list(CtFieldWrite.class)
+                .stream()
+                .filter(fw -> fw.getVariable().getSimpleName().equals(field.getSimpleName()))
+                .collect(Collectors.toList());
     }
 }
