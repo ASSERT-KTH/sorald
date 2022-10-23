88b580a4cb9447ef1b6ba681877cde2714660b40
Aman Sharma <mannu.poski10@gmail.com>
2022-10-23 23:04:04 +0300

    fix(S1068): delete field writes of violated field only (#921)
    * Modify tests
    
    * fix(S1068): delete field writes of corresponding field only
    
    * Add syntactic sugar
    
    * Add tribute

--- sorald/src/test/resources/processor_test_files/S1068_UnusedPrivateField/PrivateFieldWrittenButNotRead.java
+++ sorald/src/test/resources/processor_test_files/S1068_UnusedPrivateField/PrivateFieldWrittenButNotRead.java
@@ -3,11 +3,18 @@ public class PrivateFieldWrittenButNotRead {
 
     private String foo; // Noncompliant
 
+    private long zhang;
+
     public PrivateFieldWrittenButNotRead(int x, int y) {
         this.x = x + y;
     }
 
     public void initialize(String bar) {
         foo = bar;
+        zhang = 234324L;
+    }
+
+    public long getZhang() {
+        return zhang;
     }
 }
