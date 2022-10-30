8a65a787e60e6fe269def0b7bb5eb0b0255609b8
Aman Sharma <mannu.poski10@gmail.com>
2022-10-30 09:09:48 +0100

    Add test to keep track of deactivated rules

--- sorald/src/test/java/sorald/sonar/SonarLintEngineTest.java
+++ sorald/src/test/java/sorald/sonar/SonarLintEngineTest.java
@@ -1,6 +1,7 @@
 package sorald.sonar;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.hamcrest.io.FileMatchers.anExistingFile;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -8,11 +9,20 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.sonarsource.sonarlint.core.analysis.AnalysisEngine;
+import org.sonarsource.sonarlint.core.rule.extractor.SonarLintRuleDefinition;
 
 class SonarLintEngineTest {
+
+    private static final List<SonarRule> DEACTIVATED_RULES =
+            Stream.of("java:S1943").map(SonarRule::new).collect(Collectors.toList());
+
     @Test
     void sonarLintEngineReinitializesAnalysisEngineForEachExecution() {
         // arrange
@@ -48,5 +58,24 @@ class SonarLintEngineTest {
         // assert
         assertThat(result.getPath().toFile(), anExistingFile());
         assertFalse(result.isDownloaded());
+    }
+
+    @Test
+    void DEACTIVATED_RULES_shouldBeDeactivatedByDefault() {
+        // arrange
+        Map<String, SonarLintRuleDefinition> allRules =
+                SonarLintEngine.getAllRulesDefinitionsByKey();
+        List<SonarRule> deactivatedRules = getDeactivatedRules(allRules);
+
+        // assert
+        assertThat(deactivatedRules, hasItems(DEACTIVATED_RULES.toArray(new SonarRule[0])));
+    }
+
+    private static List<SonarRule> getDeactivatedRules(
+            Map<String, SonarLintRuleDefinition> sonarLintEngine) {
+        return sonarLintEngine.values().stream()
+                .filter(rule -> !rule.isActiveByDefault())
+                .map(rule -> new SonarRule(rule.getKey()))
+                .collect(Collectors.toList());
     }
 }
