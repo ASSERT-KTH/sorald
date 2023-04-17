910e44171b9c6134904e992c9eb007599fce35a8
Aman Sharma <mannu.poski10@gmail.com>
2023-04-17 18:03:50 +0200

    Generify package inclusion

--- experimentation/tools/setup.py
+++ experimentation/tools/setup.py
@@ -15,7 +15,7 @@ required = [
 setup(
     name="soraldscripts",
     description="Helper scripts for the Sorald automatic repair tool",
-    packages=find_packages('sorald/_helpers', 'sorald/achievements', exclude=("tests", "docs")),
+    packages=find_packages('sorald/', exclude=("tests", "docs")),
     tests_require=test_requirements,
     install_requires=required,
     extras_require=dict(TEST=test_requirements),
