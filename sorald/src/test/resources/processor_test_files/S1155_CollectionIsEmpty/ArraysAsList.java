2602fa06962b76621a49450eee1005a3b741d3e1
Aman Sharma <mannu.poski10@gmail.com>
2023-04-27 13:56:58 +0200

    Add tests for Arrays.asList

---
+++ sorald/src/test/resources/processor_test_files/S1155_CollectionIsEmpty/ArraysAsList.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class ArraysAsList {
+    void main() {
+        if (Arrays.asList("a", "b", "c").size() == 0) { // Noncompliant
+            return;
+        }
+        if (Arrays.asList("a", "b", "c").size() < 1) { // Noncompliant
+            return;
+        }
+        if (Arrays.asList("a", "b", "c").size() <= 0) { // Noncompliant
+            return;
+        }
+        if (Arrays.asList("a", "b", "c").size() > 3) { // Compliant
+            return;
+        }
+        if (Arrays.asList("a", "b", "c").size() >= 1) { // Noncompliant
+            return;
+        }
+        if (0 >= Arrays.asList("a", "b", "c").size()) { // Noncompliant
+            return;
+        }
+        if (Arrays.asList(1, 2, 3).size() >= 0) { // Noncompliant
+            return;
+        }
+    }
+}
